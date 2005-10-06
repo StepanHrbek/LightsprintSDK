615a22b4e3f3b93e6f7bef9c7c12e11ba752ab96
Stepan Hrbek <stepan.git@lightsprint.com>
2005-10-06 04:01:42 +0000

    svn168
    [2005.10.5 release]
    intersect:
    +pcube (zapomenul jsem ho pridat uz davno)
    +stdint.h (zapomenul jsem ho pridat nedavno)

---
+++ src/RRCollider/stdint.h
@@ -0,0 +1,23 @@
+// temporary replacement for ISO C99 stdint.h missing in Visual C++
+
+typedef unsigned __int8  uint8_t;
+typedef unsigned __int16 uint16_t;
+typedef unsigned __int32 uint32_t;
+typedef unsigned __int64 uint64_t;
+
+typedef signed __int8  int8_t;
+typedef signed __int16 int16_t;
+typedef signed __int32 int32_t;
+typedef signed __int64 int64_t;
+
+static union
+{
+    char    int8_t_incorrect[sizeof(  int8_t) == 1];
+    char   uint8_t_incorrect[sizeof( uint8_t) == 1];
+    
+    char   int16_t_incorrect[sizeof( int16_t) == 2];
+    char  uint16_t_incorrect[sizeof(uint16_t) == 2];
+    
+    char   int32_t_incorrect[sizeof( int32_t) == 4];
+    char  uint32_t_incorrect[sizeof(uint32_t) == 4];
+};
