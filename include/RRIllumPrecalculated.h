0578e4a056b87b56e723c491f8d341daf1697245
Stepan Hrbek <stepan.git@lightsprint.com>
2006-06-04 22:14:54 +0000

    svn449

---
+++ include/RRIllumPrecalculated.h
@@ -0,0 +1,38 @@
+#ifndef RRILLUMPRECALCULATED_H
+#define RRILLUMPRECALCULATED_H
+
+//////////////////////////////////////////////////////////////////////////////
+//! \file RRIllumPrecalculated.h
+//! \brief RRIllumPrecalculated - library for accessing precalculated illumination
+//! \version 2006.4.16
+//! \author Copyright (C) Lightsprint
+//! All rights reserved
+//////////////////////////////////////////////////////////////////////////////
+
+#include "RRMath.h"
+
+/*
+#ifdef _MSC_VER
+#	ifdef RR_STATIC
+		// use static library
+		#ifdef NDEBUG
+			#pragma comment(lib,"RRIllumPrecalculated_s.lib")
+		#else
+			#pragma comment(lib,"RRIllumPrecalculated_sd.lib")
+		#endif
+#	else
+#	ifdef RR_DLL_BUILD_PRECALCULATED
+		// build dll
+#		undef RR_API
+#		define RR_API __declspec(dllexport)
+#	else // use dll
+#pragma comment(lib,"RRIllumPrecalculated.lib")
+#	endif
+#	endif
+#endif
+*/
+
+#include "RRIllumPrecalculatedSurface.h"
+#include "RRIllumPrecalculatedSpace.h"
+
+#endif
