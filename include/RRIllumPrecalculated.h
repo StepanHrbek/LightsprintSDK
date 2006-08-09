89b3d1f4a527106c0d2aa9d3aa54987aeeffaf81
Stepan Hrbek <stepan.git@lightsprint.com>
2006-08-09 10:57:49 +0000

    svn561
    distro
    *cleanups

--- include/RRIllumPrecalculated.h
+++ include/RRIllumPrecalculated.h
@@ -9,30 +9,7 @@
 //! All rights reserved
 //////////////////////////////////////////////////////////////////////////////
 
-#include "RRMath.h"
-
-/*
-#ifdef _MSC_VER
-#	ifdef RR_STATIC
-		// use static library
-		#if defined(RR_RELEASE) || (defined(NDEBUG) && !defined(RR_DEBUG))
-			#pragma comment(lib,"RRIllumPrecalculated_sr.lib")
-		#else
-			#pragma comment(lib,"RRIllumPrecalculated_sd.lib")
-		#endif
-#	else
-#	ifdef RR_DLL_BUILD_PRECALCULATED
-		// build dll
-#		undef RR_API
-#		define RR_API __declspec(dllexport)
-#	else // use dll
-#pragma comment(lib,"RRIllumPrecalculated.lib")
-#	endif
-#	endif
-#endif
-*/
 
 #include "RRIllumPrecalculatedSurface.h"
-//!!!#include "RRIllumPrecalculatedSpace.h"
 
 #endif
