661c08fe3b7fefef446387bcbb8e602df8de1ace
Stepan Hrbek <stepan.git@lightsprint.com>
2007-01-03 19:27:16 +0000

    svn751

--- src/FCSS/shadowcast.cpp
+++ src/FCSS/shadowcast.cpp
@@ -1980,16 +1980,9 @@ void parseOptions(int argc, char **argv)
 
 int main(int argc, char **argv)
 {
-/*	// Get current flag
-	int tmpFlag = _CrtSetDbgFlag( _CRTDBG_REPORT_FLAG );
-	// Turn on leak-checking bit
-	tmpFlag |= _CRTDBG_LEAK_CHECK_DF;
-	// Turn off CRT block checking bit
-	tmpFlag &= ~_CRTDBG_CHECK_CRT_DF;
-	// Set flag to the new value
-	_CrtSetDbgFlag( tmpFlag );
-//	_crtBreakAlloc = 33935;
-*/
+	//_CrtSetDbgFlag( (_CrtSetDbgFlag( _CRTDBG_REPORT_FLAG )|_CRTDBG_LEAK_CHECK_DF)&~_CRTDBG_CHECK_CRT_DF );
+	//_crtBreakAlloc = 33935;
+
 	// check for version mismatch
 	if(!RR_INTERFACE_OK)
 	{
