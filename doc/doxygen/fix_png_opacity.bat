dd49c95e423e1f2f493db7102e4936c73a6e3d83
Stepan Hrbek <stepan.git@lightsprint.com>
2007-01-20 15:40:37 +0000

    svn778
    doxy:
    @zapomenute obrazky

---
+++ doc/doxygen/fix_png_opacity.bat
@@ -0,0 +1,6 @@
+"..\..\3rd\ImageMagick\convert.exe" -quality 100 -opaque #ffffff libraries.png libraries.png
+"..\..\3rd\ImageMagick\convert.exe" -quality 100 -opaque #ffffff Integration1.png Integration1.png
+"..\..\3rd\ImageMagick\convert.exe" -quality 100 -opaque #ffffff Integration2.png Integration2.png
+"..\..\3rd\ImageMagick\convert.exe" -quality 100 -opaque #ffffff Integration3.png Integration3.png
+"..\..\3rd\ImageMagick\convert.exe" -quality 100 -opaque #ffffff Integration4.png Integration4.png
+"..\..\3rd\ImageMagick\convert.exe" -quality 100 -opaque #ffffff Integration5.png Integration5.png
