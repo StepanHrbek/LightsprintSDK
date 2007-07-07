b654886374e6cc7f86066332639daabb70ad38f6
Stepan Hrbek <stepan.git@lightsprint.com>
2007-07-07 22:27:40 +0000

    svn1129
    doxy:
    *calculation, integration

--- doc/doxygen/fix_png_opacity.bat
+++ doc/doxygen/fix_png_opacity.bat
@@ -1,6 +1,7 @@
 rem "..\..\3rd\ImageMagick\convert.exe" -quality 100 -opaque #ffffff libraries.png libraries.png
 "..\..\3rd\ImageMagick\convert.exe" -quality 100 -opaque #ffffff Integration1a.png Integration1a.png
 "..\..\3rd\ImageMagick\convert.exe" -quality 100 -opaque #ffffff Integration1b.png Integration1b.png
+"..\..\3rd\ImageMagick\convert.exe" -quality 100 -opaque #ffffff Integration1c.png Integration1c.png
 "..\..\3rd\ImageMagick\convert.exe" -quality 100 -opaque #ffffff Integration2.png Integration2.png
 "..\..\3rd\ImageMagick\convert.exe" -quality 100 -opaque #ffffff Integration3.png Integration3.png
 "..\..\3rd\ImageMagick\convert.exe" -quality 100 -opaque #ffffff Integration4a.png Integration4a.png
