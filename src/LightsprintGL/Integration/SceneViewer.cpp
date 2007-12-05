8af9389c63d8b252ee876e3bf3f6c9dc2f49a652
Stepan Hrbek <stepan.git@lightsprint.com>
2007-12-05 11:31:29 +0000

    svn1352
    rr_gl:
    +sceneViewer zobrazuje properties

--- src/LightsprintGL/Integration/SceneViewer.cpp
+++ src/LightsprintGL/Integration/SceneViewer.cpp
@@ -39,13 +39,15 @@ void error(const char* message, bool gfxRelated)
 // globals are ugly, but required by GLUT design with callbacks
 
 class Solver*              solver = NULL;
-Camera                     eye(-1.856,1.440,2.097,2.404,0.000,0.020,1.3,110.0,0.1,1000.0);
+Camera                     eye(-1.856,1.440,2.097,2.404,0.000,0.020,1.3,90.0,0.1,1000.0);
+enum SelectionType {ST_CAMERA, ST_LIGHT, ST_OBJECT};
+SelectionType              selectedType = ST_CAMERA;
 unsigned                   selectedLightIndex = 0; // index into lights, light controlled by mouse/arrows
+unsigned                   selectedObjectIndex = 0; // index into static objects
 int                        winWidth = 0;
 int                        winHeight = 0;
 bool                       renderLights = 1;
 bool                       renderAmbient = 0;
-bool                       modeMovingEye = true;
 float                      speedGlobal = 1;
 float                      speedForward = 0;
 float                      speedBack = 0;
@@ -106,11 +108,18 @@ public:
 	{
 		// select submenu
 		int selectHandle = glutCreateMenu(selectCallback);
+		char buf[100];
 		glutAddMenuEntry("camera", -1);
 		for(unsigned i=0;i<solver->getLights().size();i++)
-			glutAddMenuEntry("light", i);
+		{
+			sprintf(buf,"light %d",i);
+			glutAddMenuEntry(buf, i);
+		}
 		for(unsigned i=0;i<solver->getStaticObjects().size();i++)
-			glutAddMenuEntry("object", 1000+i);
+		{
+			sprintf(buf,"object %d",i);
+			glutAddMenuEntry(buf, 1000+i);
+		}
 
 		// speed submenu
 		int speedHandle = glutCreateMenu(speedCallback);
@@ -147,8 +156,9 @@ public:
 	}
 	static void selectCallback(int item)
 	{
-		if(item<0) modeMovingEye = 1;
-		if(item>=0 && item<1000) {modeMovingEye = 0; selectedLightIndex = item;}
+		if(item<0) selectedType = ST_CAMERA;
+		if(item>=0 && item<1000) {selectedType = ST_LIGHT; selectedLightIndex = item;}
+		if(item>=1000) {selectedType = ST_OBJECT; selectedObjectIndex = item-1000;}
 		glutWarpPointer(winWidth/2,winHeight/2);
 	}
 	static void speedCallback(int item)
@@ -199,31 +209,23 @@ void keyboard(unsigned char c, int x, int y)
 {
 	switch (c)
 	{
-		case '+':
-			brightness *= 1.2;
-			break;
-		case '-':
-			brightness /= 1.2;
-			break;
-		case '*':
-			gamma *= 1.2;
-			break;
-		case '/':
-			gamma /= 1.2;
-			break;
+		case '+': brightness *= 1.2; break;
+		case '-': brightness /= 1.2; break;
+		case '*': gamma *= 1.2; break;
+		case '/': gamma /= 1.2; break;
 
 		case 'a':
-		case 'A': special(GLUT_KEY_LEFT,0,0); break;
+		case 'A': speedLeft = 1; break;
 		case 's':
-		case 'S': special(GLUT_KEY_DOWN,0,0); break;
+		case 'S': speedBack = 1; break;
 		case 'd':
-		case 'D': special(GLUT_KEY_RIGHT,0,0); break;
+		case 'D': speedRight = 1; break;
 		case 'w':
-		case 'W': special(GLUT_KEY_UP,0,0); break;
+		case 'W': speedForward = 1; break;
 		case 'q':
-		case 'Q': special(GLUT_KEY_PAGE_UP,0,0); break;
+		case 'Q': speedUp = 1; break;
 		case 'z':
-		case 'Z': special(GLUT_KEY_PAGE_DOWN,0,0); break;
+		case 'Z': speedDown = 1; break;
 		case 'x':
 		case 'X': speedLean = -1; break;
 		case 'c':
@@ -234,6 +236,29 @@ void keyboard(unsigned char c, int x, int y)
 		//	exit(0);
 	}
 	solver->reportInteraction();
+}
+
+void keyboardUp(unsigned char c, int x, int y)
+{
+	switch (c)
+	{
+		case 'a':
+		case 'A': speedLeft = 0; break;
+		case 's':
+		case 'S': speedBack = 0; break;
+		case 'd':
+		case 'D': speedRight = 0; break;
+		case 'w':
+		case 'W': speedForward = 0; break;
+		case 'q':
+		case 'Q': speedUp = 0; break;
+		case 'z':
+		case 'Z': speedDown = 0; break;
+		case 'x':
+		case 'X': speedLean = 0; break;
+		case 'c':
+		case 'C': speedLean = 0; break;
+	}
 }
 
 void reshape(int w, int h)
@@ -251,7 +276,10 @@ void reshape(int w, int h)
 void mouse(int button, int state, int x, int y)
 {
 	if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && solver->realtimeLights.size())
-		modeMovingEye = !modeMovingEye;
+	{
+		if(selectedType!=ST_CAMERA) selectedType = ST_CAMERA;
+		else selectedType = ST_LIGHT;
+	}
 	if(button == GLUT_WHEEL_UP && state == GLUT_UP)
 	{
 		if(eye.fieldOfView>13) eye.fieldOfView -= 10;
@@ -271,13 +299,14 @@ void passive(int x, int y)
 	y -= winHeight/2;
 	if(x || y)
 	{
-		if(modeMovingEye)
+		if(selectedType==ST_CAMERA || selectedType==ST_OBJECT)
 		{
 			eye.angle -= 0.005*x;
 			eye.angleX -= 0.005*y;
 			CLAMP(eye.angleX,-M_PI*0.49f,M_PI*0.49f);
 		}
 		else
+		if(selectedType==ST_LIGHT)
 		{
 			Camera* light = solver->realtimeLights[selectedLightIndex]->getParent();
 			light->angle -= 0.005*x;
@@ -294,6 +323,18 @@ void passive(int x, int y)
 		glutWarpPointer(winWidth/2,winHeight/2);
 		solver->reportInteraction();
 	}
+}
+
+static void textOutput(int x, int y, const char *format, ...)
+{
+	char text[1000];
+	va_list argptr;
+	va_start (argptr,format);
+	vsprintf (text,format,argptr);
+	va_end (argptr);
+	glRasterPos2i(x,y);
+	int len = (int)strlen(text);
+	for(int i=0;i<len;i++) glutBitmapCharacter(GLUT_BITMAP_9_BY_15,text[i]);
 }
 
 void display(void)
@@ -322,7 +363,10 @@ void display(void)
 
 	if(renderLights)
 	{
+		// render light frames
 		solver->renderLights();
+
+		// render lines
 		glBegin(GL_LINES);
 		enum {LINES=100, SIZE=100};
 		for(unsigned i=0;i<LINES+1;i++)
@@ -342,6 +386,165 @@ void display(void)
 			glVertex3f(+0.5*SIZE,0,q);
 		}
 		glEnd();
+
+		// render properties
+		glDisable(GL_DEPTH_TEST);
+		glMatrixMode(GL_MODELVIEW);
+		glPushMatrix();
+		glLoadIdentity();
+		glMatrixMode(GL_PROJECTION);
+		glPushMatrix();
+		glLoadIdentity();
+		gluOrtho2D(0, winWidth, winHeight, 0);
+//		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+//		glEnable(GL_BLEND);
+//		glColor4f(0.0,0.0,0.0,0.6);
+//		unsigned rectWidth = 530;
+//		unsigned rectHeight = 360;
+//		glColor4f(0.0,0.1,0.3,0.6);
+//		glRecti((winWidth+rectWidth)/2, (winHeight-rectHeight)/2, (winWidth-rectWidth)/2, (winHeight+rectHeight)/2);
+//		glDisable(GL_BLEND);
+		glColor3f(1,1,1);
+		int x = 10;
+		int y = 10;
+		textOutput(x,y+=18,"right mouse button = menu");
+		{
+			textOutput(x,y+=18*2,"[camera]");
+			textOutput(x,y+=18,"pos: %f %f %f",eye.pos[0],eye.pos[1],eye.pos[2]);
+			textOutput(x,y+=18,"dir: %f %f %f",eye.dir[0],eye.dir[1],eye.dir[2]);
+		}
+		unsigned numObjects = solver->getNumObjects();
+		unsigned numLights = solver->getLights().size();
+		const rr::RRObject* multiObject = solver->getMultiObjectCustom();
+		const rr::RRObject* singleObject = solver->getObject(selectedObjectIndex);
+		const rr::RRMesh* multiMesh = multiObject->getCollider()->getMesh();
+		const rr::RRMesh* singleMesh = singleObject->getCollider()->getMesh();
+		unsigned numTrianglesMulti = multiMesh->getNumTriangles();
+		unsigned numTrianglesSingle = singleMesh->getNumTriangles();
+		if(selectedLightIndex<solver->realtimeLights.size())
+		{
+			RealtimeLight* rtlight = solver->realtimeLights[selectedLightIndex];
+			const rr::RRLight* rrlight = rtlight->origin;
+			Camera* light = rtlight->getParent();
+			textOutput(x,y+=18*2,"[light %d/%d]",selectedLightIndex,solver->realtimeLights.size());
+			textOutput(x,y+=18,"type: %s",(rrlight->type==rr::RRLight::POINT)?"point":((rrlight->type==rr::RRLight::SPOT)?"spot":"dir"));
+			textOutput(x,y+=18,"pos: %f %f %f",light->pos[0],light->pos[1],light->pos[2]);
+			textOutput(x,y+=18,"dir: %f %f %f",light->dir[0],light->dir[1],light->dir[2]);
+			textOutput(x,y+=18,"color: %f %f %f",rrlight->color[0],rrlight->color[1],rrlight->color[2]);
+			switch(rrlight->distanceAttenuationType)			
+			{
+				case rr::RRLight::NONE:        textOutput(x,y+=18,"dist att: none"); break;
+				case rr::RRLight::PHYSICAL:    textOutput(x,y+=18,"dist att: physically correct"); break;
+				case rr::RRLight::POLYNOMIAL:  textOutput(x,y+=18,"dist att: 1/(%f+%f*d+%f*d^2)",rrlight->polynom[0],rrlight->polynom[1],rrlight->polynom[2]); break;
+				case rr::RRLight::EXPONENTIAL: textOutput(x,y+=18,"dist att: max(0,1-distance/%f)^%f)",rrlight->radius,rrlight->fallOffExponent); break;
+			}
+			static RealtimeLight* lastLight = NULL;
+			static unsigned numLightReceivers = 0;
+			static unsigned numShadowCasters = 0;
+			if(rtlight!=lastLight)
+			{
+				lastLight = rtlight;
+				numLightReceivers = 0;
+				numShadowCasters = 0;
+				for(unsigned t=0;t<numTrianglesMulti;t++)
+				{
+					if(multiObject->getTriangleMaterial(t,rrlight,NULL)) numLightReceivers++;
+					for(unsigned j=0;j<numObjects;j++)
+					{
+						if(multiObject->getTriangleMaterial(t,rrlight,solver->getObject(j))) numShadowCasters++;
+					}
+				}
+			}
+			textOutput(x,y+=18,"light receivers: %d/%d",numLightReceivers,numTrianglesMulti);
+			textOutput(x,y+=18,"shadow casters: %d/%d",numShadowCasters,numTrianglesMulti*numObjects);
+		}
+		if(selectedObjectIndex<solver->getNumObjects())
+		{
+			textOutput(x,y+=18*2,"[static object %d/%d]",selectedObjectIndex,numObjects);
+			textOutput(x,y+=18,"triangles: %d/%d",numTrianglesSingle,numTrianglesMulti);
+			textOutput(x,y+=18,"vertices: %d/%d",singleMesh->getNumVertices(),multiMesh->getNumVertices());
+			static const rr::RRObject* lastObject = NULL;
+			static unsigned numReceivedLights = 0;
+			static unsigned numShadowsCast = 0;
+			if(singleObject!=lastObject)
+			{
+				lastObject = singleObject;
+				numReceivedLights = 0;
+				numShadowsCast = 0;
+				for(unsigned i=0;i<numLights;i++)
+				{
+					rr::RRLight* rrlight = solver->getLights()[i];
+					for(unsigned t=0;t<numTrianglesSingle;t++)
+					{
+						if(singleObject->getTriangleMaterial(t,rrlight,NULL)) numReceivedLights++;
+						for(unsigned j=0;j<numObjects;j++)
+						{
+							if(singleObject->getTriangleMaterial(t,rrlight,solver->getObject(j))) numShadowsCast++;
+						}
+					}
+				}
+			}
+			textOutput(x,y+=18,"received lights: %f/%d",numReceivedLights/float(numTrianglesSingle),numLights);
+			textOutput(x,y+=18,"shadows cast: %f/%d",numShadowsCast/float(numTrianglesSingle),numLights*numObjects);
+		}
+		{
+			rr::RRRay* ray = rr::RRRay::create();
+			rr::RRVec3 dir = eye.dir.RRVec3::normalized();
+			ray->rayOrigin = eye.pos;
+			ray->rayDirInv[0] = 1/dir[0];
+			ray->rayDirInv[1] = 1/dir[1];
+			ray->rayDirInv[2] = 1/dir[2];
+			ray->rayLengthMin = 0;
+			ray->rayLengthMax = 10000;
+			ray->rayFlags = rr::RRRay::FILL_DISTANCE|rr::RRRay::FILL_PLANE|rr::RRRay::FILL_POINT2D|rr::RRRay::FILL_POINT3D|rr::RRRay::FILL_SIDE|rr::RRRay::FILL_TRIANGLE;
+			if(solver->getMultiObjectCustom()->getCollider()->intersect(ray))
+			{
+				rr::RRMesh::MultiMeshPreImportNumber preTriangle = multiMesh->getPreImportTriangle(ray->hitTriangle);
+				const rr::RRMaterial* material = multiObject->getTriangleMaterial(ray->hitTriangle,NULL,NULL);
+				rr::RRMaterial pointMaterial;
+				if(material->sideBits[ray->hitFrontSide?0:1].pointDetails)
+				{
+					multiObject->getPointMaterial(ray->hitTriangle,ray->hitPoint2d,pointMaterial);
+					material = &pointMaterial;
+				}
+				rr::RRMesh::TriangleMapping triangleMapping;
+				multiMesh->getTriangleMapping(ray->hitTriangle,triangleMapping);
+				rr::RRVec2 uvInLightmap = triangleMapping.uv[0] + triangleMapping.uv[1]*ray->hitPoint2d[0] + triangleMapping.uv[2]*ray->hitPoint2d[1];
+				textOutput(x,y+=18*2,"[point in the middle of viewport]");
+				textOutput(x,y+=18,"object: %d/%d",preTriangle.object,numObjects);
+				textOutput(x,y+=18,"triangle in object: %d/%d",preTriangle.index,numTrianglesSingle);
+				textOutput(x,y+=18,"triangle in scene: %d/%d",ray->hitTriangle,numTrianglesMulti);
+				textOutput(x,y+=18,"uv in triangle: %f %f",ray->hitPoint2d[0],ray->hitPoint2d[1]);
+				textOutput(x,y+=18,"uv in lightmap: %f %f",uvInLightmap[0],uvInLightmap[1]);
+				textOutput(x,y+=18,"distance: %f",ray->hitDistance);
+				textOutput(x,y+=18,"pos: %f %f %f",ray->hitPoint3d[0],ray->hitPoint3d[1],ray->hitPoint3d[2]);
+				textOutput(x,y+=18,"norm: %f %f %f",ray->hitPlane[0],ray->hitPlane[1],ray->hitPlane[2]);
+				textOutput(x,y+=18,"side: %s",ray->hitFrontSide?"front":"back");
+				textOutput(x,y+=18,"material: %s",(material!=&pointMaterial)?"per-triangle":"per-vertex");
+				textOutput(x,y+=18,"diffuse refl: %f %f %f",material->diffuseReflectance[0],material->diffuseReflectance[1],material->diffuseReflectance[2]);
+				textOutput(x,y+=18,"specular refl: %f",material->specularReflectance);
+				textOutput(x,y+=18,"transmittance: %f %f %f",material->specularTransmittance[0],material->specularTransmittance[1],material->specularTransmittance[2]);
+				textOutput(x,y+=18,"refraction index: %f",material->refractionIndex);
+				textOutput(x,y+=18,"dif.emittance: %f %f %f",material->diffuseEmittance[0],material->diffuseEmittance[1],material->diffuseEmittance[2]);
+				unsigned numReceivedLights = 0;
+				unsigned numShadowsCast = 0;
+				for(unsigned i=0;i<numLights;i++)
+				{
+					rr::RRLight* rrlight = solver->getLights()[i];
+					if(multiObject->getTriangleMaterial(ray->hitTriangle,rrlight,NULL)) numReceivedLights++;
+					for(unsigned j=0;j<numObjects;j++)
+					{
+						if(multiObject->getTriangleMaterial(ray->hitTriangle,rrlight,solver->getObject(j))) numShadowsCast++;
+					}
+				}
+				textOutput(x,y+=18,"received lights: %d/%d",numReceivedLights,numLights);
+				textOutput(x,y+=18,"shadows cast: %d/%d",numShadowsCast,numLights*numObjects);
+			}
+		}
+		glPopMatrix();
+		glMatrixMode(GL_MODELVIEW);
+		glPopMatrix();
+		glEnable(GL_DEPTH_TEST);
 	}
 
 	glutSwapBuffers();
@@ -359,11 +562,13 @@ void idle()
 		float seconds = (now-prev)/(float)PER_SEC;
 		CLAMP(seconds,0.001f,0.3f);
 		seconds *= speedGlobal;
-		Camera* cam = modeMovingEye?&eye:solver->realtimeLights[selectedLightIndex]->getParent();
+		Camera* cam = (selectedType!=ST_LIGHT)?&eye:solver->realtimeLights[selectedLightIndex]->getParent();
 		if(speedForward) cam->moveForward(speedForward*seconds);
 		if(speedBack) cam->moveBack(speedBack*seconds);
 		if(speedRight) cam->moveRight(speedRight*seconds);
 		if(speedLeft) cam->moveLeft(speedLeft*seconds);
+		if(speedUp) cam->moveUp(speedUp*seconds);
+		if(speedDown) cam->moveDown(speedDown*seconds);
 		if(speedLean) cam->lean(speedLean*seconds);
 		if(speedForward || speedBack || speedRight || speedLeft || speedUp || speedDown || speedLean)
 		{
@@ -397,6 +602,7 @@ void sceneViewer(rr::RRDynamicSolver* _solver, const char* pathToShaders)
 	glutSetCursor(GLUT_CURSOR_NONE);
 	glutDisplayFunc(display);
 	glutKeyboardFunc(keyboard);
+	glutKeyboardUpFunc(keyboardUp);
 	glutSpecialFunc(special);
 	glutSpecialUpFunc(specialUp);
 	glutReshapeFunc(reshape);
