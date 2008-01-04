4f3d47f7c451e745f04872ae2bab31c3f98df151
Stepan Hrbek <stepan.git@lightsprint.com>
2008-01-04 15:05:55 +0000

    svn1388
    SDK:
    *.vcproj a .sln -> .vs2005.vcproj a .vs2005.sln

--- samples/build.vs2005.bat
+++ samples/build.vs2005.bat
@@ -3,10 +3,10 @@
 call "%VS80COMNTOOLS%\vsvars32.bat" >nul
 
 echo Building VS2005 Debug DLL samples...
-devenv samples.sln /build "Debug DLL" >log
+devenv samples.vs2005.sln /build "Debug DLL" >log
 if ERRORLEVEL 1 goto error
 echo Building VS2005 Release DLL samples...
-devenv samples.sln /build "Release DLL" >log
+devenv samples.vs2005.sln /build "Release DLL" >log
 if ERRORLEVEL 1 goto error
 
 echo OK.
