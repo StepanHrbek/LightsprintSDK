d506b5f1a957fb87f65bd3e84dffc3400c7f7f24
Stepan Hrbek <stepan.git@lightsprint.com>
2008-02-29 20:22:03 +0000

    svn1568
    rr_gl:
    @dialog.h chybel uz asi 10 revizi

---
+++ src/LightsprintGL/dialog.h
@@ -0,0 +1,23 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by c:\Documents and Settings\Dee\Dokumenty\C\rr\src\win32dlg\win32dlg.rc
+//
+#define IDD_DIALOG1                     101
+#define IDC_VIEWER                      1001
+#define IDC_QUALITY                     1002
+#define IDC_LOG                         1003
+#define IDC_BUTTON2                     1004
+#define IDC_ABORT                       1004
+#define IDC_UEBUILD                     1005
+#define IDC_CUSTOM                      1005
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
