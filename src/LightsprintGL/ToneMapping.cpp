bffcf69ae5811ac042218daa1a0eef60367d0b35
Stepan Hrbek <stepan.git@lightsprint.com>
2008-05-31 22:05:19 +0000

    svn1770
    rr_gl:
    @tonemapping mozna prumeroval spatny rect (ale nevim to jiste)

--- src/LightsprintGL/ToneMapping.cpp
+++ src/LightsprintGL/ToneMapping.cpp
@@ -5,6 +5,7 @@
 
 #include <GL/glew.h>
 #include "Lightsprint/GL/ToneMapping.h"
+//#include "Lightsprint/RRDebug.h"
 
 namespace rr_gl
 {
@@ -40,17 +41,26 @@ void ToneMapping::adjustOperator(rr::RRReal secondsSinceLastAdjustment, rr::RRVe
 	if(!textureRenderer) return;
 	int viewport[4];
 	glGetIntegerv(GL_VIEWPORT,viewport);
-	if(!bigTexture || bigTexture->getBuffer()->getWidth()<(unsigned)viewport[2] || bigTexture->getBuffer()->getHeight()<(unsigned)viewport[3])
-	{
-		if(bigTexture)
-		{
-			delete bigTexture->getBuffer();
-			delete bigTexture;
-		}
-		bigTexture = new Texture(rr::RRBuffer::create(rr::BT_2D_TEXTURE,viewport[2],viewport[3],1,rr::BF_RGB,true,NULL),false,false,GL_NEAREST,GL_NEAREST,GL_REPEAT,GL_REPEAT);
-	}
+
+	// changes size of texture on CPU, GPU synchronizes to CPU, cleaner but system ram is wasted
+	//if(!bigTexture || bigTexture->getBuffer()->getWidth()<(unsigned)viewport[2] || bigTexture->getBuffer()->getHeight()<(unsigned)viewport[3])
+	//{
+	//	if(bigTexture)
+	//	{
+	//		delete bigTexture->getBuffer();
+	//		delete bigTexture;
+	//	}
+	//	bigTexture = new Texture(rr::RRBuffer::create(rr::BT_2D_TEXTURE,viewport[2],viewport[3],1,rr::BF_RGB,true,NULL),false,false,GL_NEAREST,GL_NEAREST,GL_REPEAT,GL_REPEAT);
+	//}
+	//bigTexture->bindTexture();
+	//glCopyTexSubImage2D(GL_TEXTURE_2D,0,0,0,viewport[0],viewport[1],viewport[2],viewport[3]);
+
+	// changes size of texture on GPU, makes following render2D() parameters simpler
+	// system ram is not wasted
+	if(!bigTexture) bigTexture = new Texture(rr::RRBuffer::create(rr::BT_2D_TEXTURE,1,1,1,rr::BF_RGB,true,NULL),false,false,GL_NEAREST,GL_NEAREST,GL_REPEAT,GL_REPEAT);
 	bigTexture->bindTexture();
-	glCopyTexSubImage2D(GL_TEXTURE_2D,0,0,0,viewport[0],viewport[1],viewport[2],viewport[3]);
+	glCopyTexImage2D(GL_TEXTURE_2D,0,GL_RGB,0,0,viewport[2],viewport[3],0);
+
 	const unsigned width = 64;
 	const unsigned height = 32;
 	unsigned char buf[width*height*3];
@@ -61,9 +71,11 @@ void ToneMapping::adjustOperator(rr::RRReal secondsSinceLastAdjustment, rr::RRVe
 		smallTexture = new Texture(rr::RRBuffer::create(rr::BT_2D_TEXTURE,width,height,1,rr::BF_RGB,true,NULL),false,false,GL_NEAREST,GL_NEAREST,GL_REPEAT,GL_REPEAT);
 	}
 	smallTexture->renderingToBegin();
-	textureRenderer->render2D(bigTexture,NULL,0,0,float(width)/viewport[2],float(height)/viewport[3]);
+	glViewport(0,0,width,height);
+	textureRenderer->render2D(bigTexture,NULL,0,0,1,1);
 	glReadPixels(0,0,width,height,GL_RGB,GL_UNSIGNED_BYTE,buf);
 	smallTexture->renderingToEnd();
+	glViewport(viewport[0],viewport[1],viewport[2],viewport[3]);
 	for(unsigned i=0;i<256;i++)
 		histo[i] = 0;
 	for(unsigned i=0;i<width*height*3;i++)
@@ -71,6 +83,7 @@ void ToneMapping::adjustOperator(rr::RRReal secondsSinceLastAdjustment, rr::RRVe
 	for(unsigned i=0;i<256;i++)
 		avg += histo[i]*i;
 	avg = avg/(width*height)+1;
+	//rr::RRReporter::report(rr::INF1,"%d\n",avg);
 	brightness *= pow(128.0f/avg,CLAMPED(secondsSinceLastAdjustment*0.2f,0.0002f,0.3f));
 }
 
