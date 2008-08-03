197fb29392fa2215457ea22157ecb4fac222da1d
Stepan Hrbek <stepan.git@lightsprint.com>
2008-08-03 23:02:05 +0000

    svn2128
    fcss

--- bin/install_dependencies.sh
+++ bin/install_dependencies.sh
@@ -2,8 +2,8 @@
 
 echo "This script attempts to install freeimage, glew and glut libraries."
 echo "If it fails and you can fix it, please send me updated version back."
-echo "1) yum might work on RedHat based systems (tested on Fedora9)"
+echo "1) yum might work on RedHat based systems (tested in Fedora 9)"
 yum install freeimage glew freeglut
 
-echo "2) apt-get might work on Debian based systems (tested on Ubuntu8)"
+echo "2) apt-get might work on Debian based systems (tested in Ubuntu 8.04)"
 sudo apt-get install libfreeimage3 libglew1.5 libglut3
