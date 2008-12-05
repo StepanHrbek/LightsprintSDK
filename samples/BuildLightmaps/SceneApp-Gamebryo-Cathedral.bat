64b7c1150c93a664b08e478e8dcfa8db908a8662
Stepan Hrbek <stepan.git@lightsprint.com>
2008-12-05 22:34:05 +0000

    svn2384

--- samples/BuildLightmaps/SceneApp-Gamebryo-Cathedral.bat
+++ samples/BuildLightmaps/SceneApp-Gamebryo-Cathedral.bat
@@ -1,3 +1,4 @@
-@ECHO OFF
-cd %GAMEBRYO_GI_PATH%\SceneApp\Win32\VC90
-SceneApp.exe %GAMEBRYO_GI_PATH%\Samples\Cathedral\Cathedral.gsa %GAMEBRYO_GI_PATH%\Samples\Cathedral\LightMaps
+@rem Runs Gamebryo sample with lightmaps built by Build-Gamebryo-Cathedral.bat
+
+@cd %GAMEBRYO_GI_PATH%\SceneApp\Win32\VC90
+@SceneApp.exe %GAMEBRYO_GI_PATH%\Samples\Cathedral\Cathedral.gsa %GAMEBRYO_GI_PATH%\Samples\Cathedral\LightMaps
--- samples/BuildLightmaps/SceneApp-Gamebryo-Tutorial.bat
+++ samples/BuildLightmaps/SceneApp-Gamebryo-Tutorial.bat
@@ -1,3 +1,4 @@
-@ECHO OFF
-cd %GAMEBRYO_GI_PATH%\SceneApp\Win32\VC90
-SceneApp.exe %GAMEBRYO_GI_PATH%\Samples\Tutorial\Tutorial.gsa %GAMEBRYO_GI_PATH%\Samples\Tutorial\LightMaps
+@rem Runs Gamebryo sample with lightmaps built by Build-Gamebryo-Tutorial.bat
+
+@cd %GAMEBRYO_GI_PATH%\SceneApp\Win32\VC90
+@SceneApp.exe %GAMEBRYO_GI_PATH%\Samples\Tutorial\Tutorial.gsa %GAMEBRYO_GI_PATH%\Samples\Tutorial\LightMaps
