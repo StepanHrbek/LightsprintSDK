0bfad930fa3df5f1916b5d48e0e6d3bc7443da3a
Stepan Hrbek <stepan.git@lightsprint.com>
2008-12-11 01:24:38 +0000

    svn2412
    gamebryo:
    *v cathedral nebuilduju directional a bent normaly, bylo to matouci

--- samples/BuildLightmaps/Gamebryo-Build-Cathedral.bat
+++ samples/BuildLightmaps/Gamebryo-Build-Cathedral.bat
@@ -1,6 +1,6 @@
 @echo off
 
-rem Builds standard and directional lightmaps and bent normals for Gamebryo scene Cathedral.
+rem Builds lightmaps for Gamebryo scene Cathedral.
 rem Run Gamebryo-SceneApp-Cathedral.bat to see the same lightmaps in Gamebryo sample app.
 
-BuildLightmaps.bat "%GAMEBRYO_GI_PATH%Samples/Cathedral/Cathedral.gsa" "outputpath=%GAMEBRYO_GI_PATH%Samples/Cathedral/LightMaps/" "outputext=tga" "quality=200" "maxmapsize=1024" directional bentnormals
+BuildLightmaps.bat "%GAMEBRYO_GI_PATH%Samples/Cathedral/Cathedral.gsa" "outputpath=%GAMEBRYO_GI_PATH%Samples/Cathedral/LightMaps/" "outputext=tga" "quality=200" "maxmapsize=1024"
