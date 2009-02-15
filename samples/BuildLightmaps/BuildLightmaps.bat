59e597e8273a53822f21946f0ff44c947a947f9f
Stepan Hrbek <stepan.git@lightsprint.com>
2009-02-15 16:47:26 +0000

    svn2522
    doc

--- samples/BuildLightmaps/BuildLightmaps.bat
+++ samples/BuildLightmaps/BuildLightmaps.bat
@@ -1,6 +1,7 @@
 @echo off
 
 rem Runs one of BuildLightmaps configurations, feel free to change it.
+rem Run this .bat file to display all commandline parameters.
 
 rem ..\..\bin\win32\BuildLightmaps.vs2008.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
 rem ..\..\bin\x64\BuildLightmaps.vs2008.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
