6311d54debe8df19d9d535ca734ddac6fa43f0a7
Stepan Hrbek <stepan.git@lightsprint.com>
2009-02-15 16:45:48 +0000

    svn2521
    samply:
    *prehlednejsi viceradkovy gamebryo bataky

--- samples/BuildLightmaps/Gamebryo-Build-Cathedral.bat
+++ samples/BuildLightmaps/Gamebryo-Build-Cathedral.bat
@@ -3,4 +3,9 @@
 rem Builds lightmaps for Gamebryo scene Cathedral.
 rem Run Gamebryo-SceneApp-Cathedral.bat to see the same lightmaps in Gamebryo sample app.
 
-BuildLightmaps.bat "%GAMEBRYO_GI_PATH%Samples/Cathedral/Cathedral.gsa" "outputpath=%GAMEBRYO_GI_PATH%Samples/Cathedral/LightMaps/" "outputext=tga" "quality=200" "maxmapsize=1024"
+BuildLightmaps.bat ^
+ "%GAMEBRYO_GI_PATH%Samples/Cathedral/Cathedral.gsa" ^
+ "outputpath=%GAMEBRYO_GI_PATH%Samples/Cathedral/LightMaps/" ^
+ "outputext=tga" ^
+ "quality=200" ^
+ "maxmapsize=1024"
--- samples/BuildLightmaps/Gamebryo-Build-Tutorial.bat
+++ samples/BuildLightmaps/Gamebryo-Build-Tutorial.bat
@@ -3,4 +3,8 @@
 rem Builds standard lightmaps for Gamebryo scene Tutorial.
 rem Run Gamebryo-SceneApp-Tutorial.bat to see the same lightmaps in Gamebryo sample app.
 
-BuildLightmaps.bat "%GAMEBRYO_GI_PATH%Samples/Tutorial/Tutorial.gsa" "outputpath=%GAMEBRYO_GI_PATH%Samples/Tutorial/LightMaps/" "outputext=tga" "quality=1000"
+BuildLightmaps.bat ^
+ "%GAMEBRYO_GI_PATH%Samples/Tutorial/Tutorial.gsa" ^
+ "outputpath=%GAMEBRYO_GI_PATH%Samples/Tutorial/LightMaps/" ^
+ "outputext=tga" ^
+ "quality=1000"
