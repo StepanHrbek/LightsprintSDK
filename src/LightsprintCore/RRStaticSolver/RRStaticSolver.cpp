abbe74e1415185f8e9b15330742445ca6b4aef79
Stepan Hrbek <stepan.git@lightsprint.com>
2009-04-28 09:21:27 +0000

    svn2724
    core:
    *static solver getTriangleMeasure(exiting) pricita emisivitu, vypada to logicteji
     jde o kod ktery asi stejne nikdo nepouziva

--- src/LightsprintCore/RRStaticSolver/RRStaticSolver.cpp
+++ src/LightsprintCore/RRStaticSolver/RRStaticSolver.cpp
@@ -226,7 +226,7 @@ bool RRStaticSolver::getTriangleMeasure(unsigned triangle, unsigned vertex, RRRa
 		// measure exiting
 		if (measure.exiting)
 		{
-			irrad *= tri->surface->diffuseReflectance.color;
+			irrad = irrad * tri->surface->diffuseReflectance.color + tri->surface->diffuseEmittance.color;
 		}
 	}
 	else
