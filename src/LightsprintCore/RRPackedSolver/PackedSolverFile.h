1656c9207904fba0c8367b86aa0ccb9c3e7aa6d4
Stepan Hrbek <stepan.git@lightsprint.com>
2009-05-05 19:44:52 +0000

    svn2763
    core:
    *zvysena verze fireballu aby prepsal vsechny firebally vyrobeny od 2741 pred 3 dny (verzi ma jen cadprojekt a triana)

--- src/LightsprintCore/RRPackedSolver/PackedSolverFile.h
+++ src/LightsprintCore/RRPackedSolver/PackedSolverFile.h
@@ -11,7 +11,7 @@
 
 #define FACTOR_FORMAT 2 // 0: 32bit int/float overlap (lightsmark); 1: 32bit short+short; 2: 64bit int+float (SDK)
 
-#define FIREBALL_STRUCTURE_VERSION (7+FACTOR_FORMAT) // change when file structere changes, old files will be overwritten
+#define FIREBALL_STRUCTURE_VERSION (8+FACTOR_FORMAT) // change when file structere changes, old files will be overwritten
 #define FIREBALL_FILENAME_VERSION  2 // change when file structere changes, old files will be preserved
 
 #include <cstdio> // save/load
