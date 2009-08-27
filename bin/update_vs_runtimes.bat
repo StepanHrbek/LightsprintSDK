ec137f5cd5e39af67ce5d559501ba1b20edda3d1
Stepan Hrbek <stepan.git@lightsprint.com>
2009-08-27 16:40:47 +0000

    svn2977
    sdk:
    @vs runtimy v SDK nebyly aktualni, microsoft je pred 2 mesici hotfixnul

---
+++ bin/update_vs_runtimes.bat
@@ -0,0 +1,13 @@
+rem Updates SDK runtimes from VS runtimes. Must be updated when Microsoft updates VS runtimes (service pack, hotfix..)
+
+xcopy /D /S /Y "C:\Program Files (x86)\Microsoft Visual Studio 8\VC\redist\x86\Microsoft.VC80.CRT"        win32\Microsoft.VC80.CRT
+xcopy /D /S /Y "C:\Program Files (x86)\Microsoft Visual Studio 8\VC\redist\x86\Microsoft.VC80.OPENMP"     win32\Microsoft.VC80.OPENMP
+
+xcopy /D /S /Y "C:\Program Files (x86)\Microsoft Visual Studio 8\VC\redist\amd64\Microsoft.VC80.CRT"      x64\Microsoft.VC80.CRT
+xcopy /D /S /Y "C:\Program Files (x86)\Microsoft Visual Studio 8\VC\redist\amd64\Microsoft.VC80.OPENMP"   x64\Microsoft.VC80.OPENMP
+
+xcopy /D /S /Y "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.CRT"      win32\Microsoft.VC90.CRT
+xcopy /D /S /Y "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.OpenMP"   win32\Microsoft.VC90.OpenMP
+
+xcopy /D /S /Y "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\redist\amd64\Microsoft.VC90.CRT"    x64\Microsoft.VC90.CRT
+xcopy /D /S /Y "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\redist\amd64\Microsoft.VC90.OpenMP" x64\Microsoft.VC90.OpenMP
