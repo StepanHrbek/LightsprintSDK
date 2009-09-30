63888a2c8e6393a56c07207ab3ef3de5be5178c6
Stepan Hrbek <stepan.git@lightsprint.com>
2009-09-30 13:03:38 +0000

    svn3056
    renderlights:
    @zapomnel jsem pridat do svn pred nekolika dny

---
+++ src/LightsprintGL/SceneViewer/SVMeasuring.cpp
@@ -0,0 +1,9 @@
+// --------------------------------------------------------------------------
+// Scene viewer - measuring.
+// Copyright (C) 2009 Stepan Hrbek, Lightsprint. All rights reserved.
+// --------------------------------------------------------------------------
+
+#include "SVMeasuring.h"
+
+#ifdef SUPPORT_SCENEVIEWER
+#endif // SUPPORT_SCENEVIEWER
---
+++ src/LightsprintGL/SceneViewer/SVMeasuring.h
@@ -0,0 +1,14 @@
+// --------------------------------------------------------------------------
+// Scene viewer - measuring.
+// Copyright (C) 2009 Stepan Hrbek, Lightsprint. All rights reserved.
+// --------------------------------------------------------------------------
+
+#ifndef SVMEASURING_H
+#define SVMEASURING_H
+
+#include "Lightsprint/GL/SceneViewer.h"
+
+#ifdef SUPPORT_SCENEVIEWER
+#endif // SUPPORT_SCENEVIEWER
+
+#endif
