d4f7f73a47b263e7f2791c9b2d5926fc02f1053f
Stepan Hrbek <stepan.git@lightsprint.com>
2009-11-16 14:25:25 +0000

    svn3219
    rr_gl:
    @pole 1x1 textur jsem mazal v kazdym snimku

--- src/LightsprintGL/RendererOfRRObject.cpp
+++ src/LightsprintGL/RendererOfRRObject.cpp
@@ -16,96 +16,10 @@ namespace rr_gl
 
 //////////////////////////////////////////////////////////////////////////////
 //
-// 1x1 textures
-
-static rr::RRBuffer* buffer1x1[5] = {NULL,NULL,NULL,NULL,NULL};
-
-static void bindPropertyTexture(const rr::RRMaterial::Property& property,unsigned index)
-{
-	rr::RRBuffer* buffer = property.texture;
-	if (!buffer)
-	{
-		if (!buffer1x1[index])
-			buffer1x1[index] = rr::RRBuffer::create(rr::BT_2D_TEXTURE,1,1,1,(index==2)?rr::BF_RGBA:rr::BF_RGB,true,NULL); // 2 = RGBA
-		buffer1x1[index]->setElement(0,rr::RRVec4(property.color,1-property.color.avg()));
-		buffer = buffer1x1[index];
-	}
-	getTexture(buffer)->bindTexture();
-}
-
-static void free1x1()
-{
-	for (unsigned i=0;i<5;i++)
-		RR_SAFE_DELETE(buffer1x1[i]);
-}
-
-unsigned g_numRenderers = 0;
-
-
-//////////////////////////////////////////////////////////////////////////////
-//
-// RenderedChannels
-
-void RendererOfRRObject::RenderedChannels::useMaterial(Program* program, const rr::RRMaterial* material)
-{
-	if (!program)
-	{
-		RR_LIMITED_TIMES(1,rr::RRReporter::report(rr::ERRO,"useMaterial(): program=NULL\n"));
-		return;
-	}
-	if (!material)
-	{
-		RR_LIMITED_TIMES(1,rr::RRReporter::report(rr::WARN,"useMaterial(): material=NULL\n"));
-		rr::RRMaterial s_material;
-		RR_LIMITED_TIMES(1,s_material.reset(false));
-		material = &s_material;
-	}
-	if (MATERIAL_DIFFUSE_CONST)
-	{
-		program->sendUniform("materialDiffuseConst",material->diffuseReflectance.color[0],material->diffuseReflectance.color[1],material->diffuseReflectance.color[2],1.0f);
-	}
-
-	if (MATERIAL_SPECULAR_CONST)
-	{
-		program->sendUniform("materialSpecularConst",material->specularReflectance.color[0],material->specularReflectance.color[1],material->specularReflectance.color[2],1.0f);
-	}
-
-	if (MATERIAL_EMISSIVE_CONST)
-	{
-		program->sendUniform("materialEmissiveConst",material->diffuseEmittance.color[0],material->diffuseEmittance.color[1],material->diffuseEmittance.color[2],0.0f);
-	}
-
-	if (MATERIAL_TRANSPARENCY_CONST)
-	{
-		program->sendUniform("materialTransparencyConst",material->specularTransmittance.color[0],material->specularTransmittance.color[1],material->specularTransmittance.color[2],1-material->specularTransmittance.color.avg());
-	}
-
-	if (MATERIAL_DIFFUSE_MAP)
-	{
-		glActiveTexture(GL_TEXTURE0+TEXTURE_2D_MATERIAL_DIFFUSE);
-		bindPropertyTexture(material->diffuseReflectance,0);
-	}
-
-	if (MATERIAL_EMISSIVE_MAP)
-	{
-		glActiveTexture(GL_TEXTURE0+TEXTURE_2D_MATERIAL_EMISSIVE);
-		bindPropertyTexture(material->diffuseEmittance,1);
-	}
-
-	if (MATERIAL_TRANSPARENCY_MAP)
-	{
-		glActiveTexture(GL_TEXTURE0+TEXTURE_2D_MATERIAL_TRANSPARENCY);
-		bindPropertyTexture(material->specularTransmittance,2); // 2 = RGBA
-	}
-}
-
-//////////////////////////////////////////////////////////////////////////////
-//
 // RendererOfRRObject
 
 RendererOfRRObject::RendererOfRRObject(const rr::RRObject* _object, rr::RRDynamicSolver* _solver)
 {
-	g_numRenderers++;
 	RR_ASSERT(_object);
 	params.program = NULL;
 	params.object = _object;
@@ -129,7 +43,6 @@ RendererOfRRObject::RendererOfRRObject(const rr::RRObject* _object, rr::RRDynami
 
 RendererOfRRObject::~RendererOfRRObject()
 {
-	if (!--g_numRenderers) free1x1();
 }
 
 void RendererOfRRObject::setProgram(Program* program)
