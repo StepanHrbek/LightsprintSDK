5e709043d90627b3a5aeacf5b6dd0f05abb10e37
Stepan Hrbek <stepan.git@lightsprint.com>
2009-12-17 03:10:46 +0000

    svn3318
    rr_io:
    @dvojnasobny delete v collade, zpusobeno minulym komitem

--- src/LightsprintIO/ImportCollada/RRObjectCollada.cpp
+++ src/LightsprintIO/ImportCollada/RRObjectCollada.cpp
@@ -610,7 +610,7 @@ private:
 		if (material.diffuseReflectance.texture && !material.specularTransmittance.texture && material.diffuseReflectance.texture->getFormat()==BF_RGBA)
 		{
 			RRReporter::report(WARN,"Transparency in diffuse map. Enabling workaround for Google Sketch Up bug.\n");
-			material.specularTransmittance.texture = material.diffuseReflectance.texture;
+			material.specularTransmittance.texture = material.diffuseReflectance.texture->createReference();
 			material.specularTransmittance.texcoord = material.diffuseReflectance.texcoord;
 			material.specularTransmittanceInAlpha = true;
 		}
