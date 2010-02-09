a6f9091fee33849127f92b1e8635d50498dd5b50
Stepan Hrbek <stepan.git@lightsprint.com>
2010-02-09 16:12:45 +0000

    svn3480
    renderlights:
    +playVideos uklada do rlproj

--- src/LightsprintGL/SceneViewer/SVSaveLoad.cpp
+++ src/LightsprintGL/SceneViewer/SVSaveLoad.cpp
@@ -233,6 +233,7 @@ void save(Archive & ar, const rr_gl::SceneViewerStateEx& a, const unsigned int v
 	ar & make_nvp("renderLogo",a.renderLogo);
 	ar & make_nvp("renderTonemapping",a.renderTonemapping);
 	ar & make_nvp("adjustTonemapping",a.adjustTonemapping);
+	ar & make_nvp("playVideos",a.playVideos);
 	ar & make_nvp("cameraDynamicNear",a.cameraDynamicNear);
 	ar & make_nvp("cameraMetersPerSecond",a.cameraMetersPerSecond);
 	ar & make_nvp("brightness",a.brightness);
@@ -276,6 +277,7 @@ void load(Archive& ar, rr_gl::SceneViewerStateEx& a, const unsigned int version)
 	ar & make_nvp("renderLogo",a.renderLogo);
 	if (version>0) ar & make_nvp("renderTonemapping",a.renderTonemapping);
 	ar & make_nvp("adjustTonemapping",a.adjustTonemapping);
+	if (version>1) ar & make_nvp("playVideos",a.playVideos);
 	ar & make_nvp("cameraDynamicNear",a.cameraDynamicNear);
 	ar & make_nvp("cameraMetersPerSecond",a.cameraMetersPerSecond);
 	ar & make_nvp("brightness",a.brightness);
@@ -318,7 +320,7 @@ BOOST_SERIALIZATION_SPLIT_FREE(rr_gl::Camera)
 BOOST_SERIALIZATION_SPLIT_FREE(rr_gl::SceneViewerStateEx)
 
 BOOST_CLASS_VERSION(rr::RRLight, 1)
-BOOST_CLASS_VERSION(rr_gl::SceneViewerStateEx, 1)
+BOOST_CLASS_VERSION(rr_gl::SceneViewerStateEx, 2)
 
 //---------------------------------------------------------------------------
 
