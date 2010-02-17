01c0e10b74df0eb782e48d8d352845ab33c14bb7
Stepan Hrbek <stepan.git@lightsprint.com>
2010-02-17 15:12:02 +0000

    svn3504
    rr_io:
    @patri k minulemu komitu

---
+++ src/LightsprintIO/ImportOpenCollada/RRObjectOpenCollada.cpp
@@ -0,0 +1,16 @@
+// --------------------------------------------------------------------------
+// Lightsprint adapter for OpenCollada scenes.
+// Copyright (C) 2010 Lightsprint. All rights reserved.
+// --------------------------------------------------------------------------
+
+#include "../supported_formats.h"
+#ifdef SUPPORT_OPENCOLLADA
+
+// TODO sem prijde tak 20-40kB tveho kodu
+
+void registerLoaderOpenCollada()
+{
+//	RRScene::registerLoader("*.dae",RRSceneOpenCollada::load);
+}
+
+#endif // SUPPORT_OPENCOLLADA
---
+++ src/LightsprintIO/ImportOpenCollada/RRObjectOpenCollada.h
@@ -0,0 +1,14 @@
+// --------------------------------------------------------------------------
+// Lightsprint adapter for OpenCollada scenes.
+// Copyright (C) 2010 Lightsprint. All rights reserved.
+// --------------------------------------------------------------------------
+
+#ifndef RROBJECTOPENCOLLADA_H
+#define RROBJECTOPENCOLLADA_H
+
+#include "Lightsprint/RRScene.h"
+
+//! Makes it possible to load .dae scenes from disk via rr::RRScene::RRScene().
+void registerLoaderOpenCollada();
+
+#endif
