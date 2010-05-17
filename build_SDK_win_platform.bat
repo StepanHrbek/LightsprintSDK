7462e38e2c4409bcc88b024f04cc75e76aff1a5f
Stepan Hrbek <stepan.git@lightsprint.com>
2010-05-17 23:35:59 +0000

    svn3694
    build:
    @build_SDK_win.bat rozbity v minulem komitu
    *zrychleny (paralelni) a zjednoduseny clean_SDK_win.bat

--- build_SDK_win_platform.bat
+++ build_SDK_win_platform.bat
@@ -1,30 +1,29 @@
 @echo off
-set COMPILER=%1
-set TARGET=%2
-set SETENVBAT=%3
-set ERR=build_SDK_win_%COMPILER%_%TARGET%.err
-if NOT x%TARGET%==x- set TARGET2=|%TARGET%
+set ACTION=%1
+set COMPILER=%2
+set TARGET=%3
+set SETENVBAT=%4
+set ERR=%ACTION%_SDK_win_%COMPILER%_%TARGET%.err
+if NOT x%TARGET%==x- set TARGET2=^|%TARGET%
 call %SETENVBAT% >nul
 
-echo Building %COMPILER% Debug DLL%TARGET2%...
-devenv src\RR.%COMPILER%.sln /build "Debug DLL%TARGET2%" >%ERR%
+echo %ACTION%ing %COMPILER% "Debug DLL%TARGET2%"...
+devenv.exe src\RR.%COMPILER%.sln /%ACTION% "Debug DLL%TARGET2%" >%ERR%
 if ERRORLEVEL 1 goto error
 
-echo Building %COMPILER% Release DLL%TARGET2%...
-devenv src\RR.%COMPILER%.sln /build "Release DLL%TARGET2%" >%ERR%
+echo %ACTION%ing %COMPILER% "Release DLL%TARGET2%"...
+devenv.exe src\RR.%COMPILER%.sln /%ACTION% "Release DLL%TARGET2%" >%ERR%
 if ERRORLEVEL 1 goto error
 
 if %COMPILER%==vs2008 if %TARGET%==win32 (
-	echo Building %COMPILER% Shipping DLL%TARGET2%"...
-	devenv src\RR.%COMPILER%.sln /build "Shipping DLL%TARGET2%"" >%ERR%
+	echo %ACTION%ing %COMPILER% "Shipping DLL%TARGET2%"...
+	devenv.exe src\RR.%COMPILER%.sln /%ACTION% "Shipping DLL%TARGET2%"" >%ERR%
 	if ERRORLEVEL 1 goto error
 )
 
 del %ERR%
-echo SDK/win built OK.
-goto end
+echo OK, done.
+exit
 
 :error
 echo !!!!!!!!!!!!!!!!!!!!!!! ERROR (see %ERR%) !!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-:end
