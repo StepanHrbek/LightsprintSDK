a178ac6929e29955d8eff045f47719873224cdfb
Stepan Hrbek <stepan.git@lightsprint.com>
2010-05-27 15:52:38 +0000

    svn3738
    doc

--- src/LightsprintIO/ImportDirectShow/dxtrans.h
+++ src/LightsprintIO/ImportDirectShow/dxtrans.h
@@ -1,5 +1,5 @@
-// what's this?
-// see http://social.msdn.microsoft.com/forums/en-US/windowssdk/thread/ed097d2c-3d68-4f48-8448-277eaaf68252/
+// This file is a workaround for Windows SDK missing file.
+// It is explained at http://social.msdn.microsoft.com/forums/en-US/windowssdk/thread/ed097d2c-3d68-4f48-8448-277eaaf68252/
 
 #define __IDxtCompositor_INTERFACE_DEFINED__
 #define __IDxtAlphaSetter_INTERFACE_DEFINED__
