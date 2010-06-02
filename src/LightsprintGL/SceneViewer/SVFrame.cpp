b8fac110dab8c7afa1d9055e9217e1eef4598c8a
Stepan Hrbek <stepan.git@lightsprint.com>
2010-06-02 16:34:30 +0000

    svn3774
    sceneviewer:
    @nesel zkompilovat, rozbito dnes revizi 3771

--- src/LightsprintGL/SceneViewer/SVFrame.cpp
+++ src/LightsprintGL/SceneViewer/SVFrame.cpp
@@ -144,7 +144,7 @@ static bool getBrightness(wxWindow* parent, rr::RRVec4& brightness)
 }
 
 	#define APP_NAME wxString("SceneViewer")
-	#define APP_VERSION __DATE__
+	#define APP_VERSION_TITLE __DATE__
 
 void SVFrame::UpdateTitle()
 {
