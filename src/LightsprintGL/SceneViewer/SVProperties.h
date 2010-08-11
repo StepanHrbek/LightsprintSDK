4716e83a73b22faf8c997465d6ca776da705c7ee
Stepan Hrbek <stepan.git@lightsprint.com>
2010-08-11 15:07:22 +0000

    svn3929
    sceneviewer:
    +zakomentovany zacatek prace na properties v tabech (cekam na dalsi instrukce)

--- src/LightsprintGL/SceneViewer/SVProperties.h
+++ src/LightsprintGL/SceneViewer/SVProperties.h
@@ -8,9 +8,18 @@
 
 #ifdef SUPPORT_SCENEVIEWER
 
+//#define PROPERTYGRID_TABS
+
 #include "Lightsprint/GL/SceneViewer.h"
 #include "SVFrame.h"
 #include "wx/propgrid/propgrid.h"
+
+#ifdef PROPERTYGRID_TABS
+	#include "wx/propgrid/manager.h"
+	#define PROPERTYGRID_BASE wxPropertyGridPage
+#else
+	#define PROPERTYGRID_BASE wxPropertyGrid
+#endif
 
 namespace rr_gl
 {
@@ -31,6 +40,23 @@ namespace rr_gl
 		SVFrame* svframe; // we must remember our main frame, reading it from parent is not reliable, wxAUI may reparent us
 		SceneViewerStateEx& svs; // shortcut for svframe->svs
 	};
+
+#ifdef PROPERTYGRID_TABS
+	// Code shared by all property panels (SVXxxProperties).
+	class SVPropertiesPage : public wxPropertyGridPage
+	{
+	public:
+		SVPropertiesPage(SVFrame* _svframe)
+		: wxPropertyGridPage(), svs(_svframe->svs)
+		{
+			svframe = _svframe;
+		}
+
+	protected:
+		SVFrame* svframe; // we must remember our main frame, reading it from parent is not reliable, wxAUI may reparent us
+		SceneViewerStateEx& svs; // shortcut for svframe->svs
+	};
+#endif
 
 }; // namespace
 
