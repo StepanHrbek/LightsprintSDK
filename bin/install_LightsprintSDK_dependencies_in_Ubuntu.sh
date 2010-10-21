ce11787a58a2d5168885f1b3392954ba9faa9522
Stepan Hrbek <stepan.git@lightsprint.com>
2010-10-21 12:36:37 +0000

    svn4165
    @linux

---
+++ bin/install_LightsprintSDK_dependencies_in_Ubuntu.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+
+# Install Lightsprint SDK dependencies in Ubuntu 10.10.
+sudo apt-get install g++ libboost-dev libboost-serialization-dev libboost-filesystem-dev libboost-iostreams-dev libglew-dev libfreeimage-dev libglut3-dev
