1f9f0105c366831ff7e45acf36d5b570dbe03437
Stepan Hrbek <stepan.git@lightsprint.com>
2011-08-16 13:15:50 +0000

    svn5006
    release pro xpec, giantsparrow

--- build_SDK.bat
+++ build_SDK.bat
@@ -1,6 +1,7 @@
 start build_SDK_doc
-start build_SDK_linux32
-start build_SDK_linux64
+rem start build_SDK_linux32
+rem start build_SDK_linux64
 rem start build_SDK_ps3
 call clean_SDK_win
+pause
 start build_SDK_win
