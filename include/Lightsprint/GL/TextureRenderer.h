e626b0e02b95c069379f11c094cc6659c7ec0a01
Stepan Hrbek <stepan.git@lightsprint.com>
2012-04-16 16:59:58 +0000

    svn5487
    doc

--- include/Lightsprint/GL/TextureRenderer.h
+++ include/Lightsprint/GL/TextureRenderer.h
@@ -42,14 +42,26 @@ public:
 	//! Color is finally gamma corrected by gamma, 1 = no correction.
 	bool renderEnvironment(const rr::RRCamera& camera, const Texture* texture0, const Texture* texture1, float blendFactor, const rr::RRVec4* brightness, float gamma, bool allowDepthTest);
 
-	//! Renders 2d texture into rectangle.
+	//! Renders 2d texture into rectangle, using current blending and alpha testing modes.
 	//
-	//! x/y/w/h are in 0..1 space; 0,0 and x,y are top left corner.
-	//! For non-NULL color, texture color is multiplied by color.
-	//! \n render2D() internally calls render2dBegin() + render2dQuad() + render2dEnd().
+	//! render2D() internally calls render2dBegin() + render2dQuad() + render2dEnd().
 	//! For rendering N textures, it's possible to simply call render2D() N times,
 	//! but render2dBegin() + N*render2dQuad() + render2dEnd() is slightly faster.
-	void render2D(const Texture* texture, const rr::RRVec4* color, float gamma, float x,float y,float w,float h);
+	//! \param texture
+	//!  Texture to be rendered.
+	//! \param color
+	//!  For non-NULL color, output (texture color) is multiplied by color.
+	//! \param gamma
+	//!  Gamma correction, 1 for no change.
+	//! \param x
+	//!  Position of texture's left side in render target, 0=leftmost, 1=rightmost.
+	//! \param y
+	//!  Position of texture's bottom side in render target, 0=bottom, 1=top.
+	//! \param w
+	//!  x+w is position of texture's right side in render target, 0=leftmost, 1=rightmost. Negative w is supported.
+	//! \param h
+	//!  y+h is position of texture's top side in render target, 0=bottom, 1=top. Negative h is supported.
+	void render2D(const Texture* texture, const rr::RRVec4* color, float gamma, float x,float y,float w,float h,float z=-1);
 
 	//! Component of render2D(), initializes pipeline.
 	bool render2dBegin(const rr::RRVec4* color, float gamma);
