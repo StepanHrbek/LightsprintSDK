b81b5795307b536b99a857ceb092568644d66c68
Stepan Hrbek <stepan.git@lightsprint.com>
2012-12-20 14:16:15 +0000

    svn5959
    vs2012:
    +funguje clean a build z commandlajny/bataku

--- build_SDK_win_platform.bat
+++ build_SDK_win_platform.bat
@@ -6,16 +6,18 @@ set COMPILER=%2
 set TARGET=%3
 set SETENVBAT=%4
 set ERR=%ACTION%_SDK_win_%COMPILER%_%TARGET%.err
-if NOT x%TARGET%==x- set TARGET2=^|%TARGET%
+set TARGET2=^|%TARGET%
 call %SETENVBAT% >nul
 
 echo %ACTION%ing %COMPILER% "Debug DLL%TARGET2%"...
 rem Nesmim volat primo devenv.exe, nedostal bych z nej stdout a exitcode.
-devenv src\RR.%COMPILER%.sln /%ACTION% "Debug DLL%TARGET2%" >%ERR%
+if NOT %COMPILER%==vs2012 devenv  src\RR.%COMPILER%.sln /%ACTION% "Debug DLL%TARGET2%" >%ERR%
+if     %COMPILER%==vs2012 msbuild src\RR.%COMPILER%.sln /t:%ACTION% /p:Configuration="Debug DLL" /p:Platform=%TARGET% >%ERR%
 if ERRORLEVEL 1 goto error
 
 echo %ACTION%ing %COMPILER% "Release DLL%TARGET2%"...
-devenv src\RR.%COMPILER%.sln /%ACTION% "Release DLL%TARGET2%" >%ERR%
+if NOT %COMPILER%==vs2012 devenv  src\RR.%COMPILER%.sln /%ACTION% "Release DLL%TARGET2%" >%ERR%
+if     %COMPILER%==vs2012 msbuild src\RR.%COMPILER%.sln /t:%ACTION% /p:Configuration="Release DLL" /p:Platform=%TARGET% >%ERR%
 if ERRORLEVEL 1 goto error
 
 if %COMPILER%==vs2008 if %TARGET%==win32 (
