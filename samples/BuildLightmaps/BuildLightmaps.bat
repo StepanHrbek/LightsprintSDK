9d2af89ed87c237e4547348566341b8b33e8bc12
Stepan Hrbek <stepan.git@lightsprint.com>
2013-06-29 07:02:13 +0000

    svn6171
    samply:
    @buildlightmaps bataky volaly 2005 binarky, pritom uz asi rok mam jen 2012

--- samples/BuildLightmaps/BuildLightmaps.bat
+++ samples/BuildLightmaps/BuildLightmaps.bat
@@ -3,6 +3,5 @@
 rem Runs one of BuildLightmaps configurations, feel free to change it.
 rem Run this .bat file to display all commandline parameters.
 
-rem ..\..\bin\win32\BuildLightmaps.vs2008.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
-rem ..\..\bin\x64\BuildLightmaps.vs2008.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
-..\..\bin\win32\BuildLightmaps.vs2005.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+rem ..\..\bin\x64\BuildLightmaps.vs2012.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+..\..\bin\win32\BuildLightmaps.vs2012.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
