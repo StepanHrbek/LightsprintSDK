2c2044f9af7d0c22aff76829ac36fe6197f0a443
Stepan Hrbek <stepan.git@lightsprint.com>
2013-12-02 12:08:02 +0000

    svn6406
    core:
    @rev6362 pred mesicem rozbil vc2005 a vc2008 kompilaci

--- src/LightsprintCore/RRCamera.cpp
+++ src/LightsprintCore/RRCamera.cpp
@@ -813,7 +813,194 @@ void RRCamera::blendAkima(unsigned numSamples, const RRCamera** samples, float* 
 	updateProjection();
 }
 
-#else
+#else // !RR_HAS_LAMBDAS
 
 void RRMatrix3x4::blendAkima(unsigned numSamples, const RRMatrix3x4** samples, const RRReal* times, RRReal time)
 {
+	RR_LIMITED_TIMES(1,RRReporter::report(WARN,"blendAkima() not supported by this compiler, requires lambdas.\n"));
+}
+void RRLight::blendAkima(unsigned numSamples, const RRLight** samples, const RRReal* times, RRReal time)
+{
+	RR_LIMITED_TIMES(1,RRReporter::report(WARN,"blendAkima() not supported by this compiler, requires lambdas.\n"));
+}
+void RRCamera::blendAkima(unsigned numSamples, const RRCamera** samples, float* times, float time)
+{
+	RR_LIMITED_TIMES(1,RRReporter::report(WARN,"blendAkima() not supported by this compiler, requires lambdas.\n"));
+}
+
+#endif // !RR_HAS_LAMBDAS
+
+
+////////////////////////////////////////////////////////////////////////
+//
+// other tools
+
+RRVec2 RRCamera::getPositionInWindow(RRVec3 worldPosition) const
+{
+	double tmp[4],out[4];
+	for (int i=0; i<4; i++) tmp[i] = worldPosition[0] * viewMatrix[0*4+i] + worldPosition[1] * viewMatrix[1*4+i] + worldPosition[2] * viewMatrix[2*4+i] + viewMatrix[3*4+i];
+	for (int i=0; i<4; i++) out[i] = tmp[0] * projectionMatrix[0*4+i] + tmp[1] * projectionMatrix[1*4+i] + tmp[2] * projectionMatrix[2*4+i] + tmp[3] * projectionMatrix[3*4+i];
+	return RRVec2((RRReal)(out[0]/out[3]),(RRReal)(out[1]/out[3]));
+}
+
+RRVec3 RRCamera::getRayOrigin(RRVec2 posInWindow) const
+{
+	if (!orthogonal)
+		return pos;
+	return
+		pos
+		+ getRight() * (posInWindow[0]+screenCenter[0]) * orthoSize * aspect
+		- getUp()    * (posInWindow[1]-screenCenter[1]) * orthoSize
+		;
+}
+
+RRVec3 RRCamera::getRayDirection(RRVec2 posInWindow) const
+{
+	if (orthogonal)
+		return getDirection();
+	// CameraObjectDistance uses length of our result, don't normalize
+	return
+		getDirection()
+		+ getRight() * ( (posInWindow[0]+screenCenter[0]) * tan(getFieldOfViewHorizontalRad()/2) )
+		- getUp()    * ( (posInWindow[1]-screenCenter[1]) * tan(getFieldOfViewVerticalRad()  /2) )
+		;
+}
+
+bool RRCamera::operator==(const RRCamera& a) const
+{
+	return pos==a.pos
+		&& yawPitchRollRad==a.yawPitchRollRad
+		//&& aspect==a.aspect ... aspect is usually just byproduct of window size, users don't want "scene was modified" just because window size changed
+		&& fieldOfViewVerticalDeg==a.fieldOfViewVerticalDeg
+		&& anear==a.anear
+		&& afar==a.afar;
+}
+
+bool RRCamera::operator!=(const RRCamera& a) const
+{
+	return !(*this==a);
+}
+
+void RRCamera::mirror(const rr::RRVec4& plane)
+{
+	// RRMatrix3x4::mirror flips left-right, but manipulateViewBy reverts it
+	//   because such flip can't be represented by pos+yawPitchRollRad
+	//   also, it would probably require CW-CCW swap while rendering, which is unwanted complication
+	//   workaround is simple and it costs nothing,
+	//    1) we flip screenCenter.x now
+	//    2) we flip left-right later when applying mirror texture
+	manipulateViewBy(RRMatrix3x4::mirror(plane));
+	screenCenter.x = -screenCenter.x;
+	updateProjection();
+}
+
+static unsigned makeFinite(float& f, float def)
+{
+	if (_finite(f))
+		return 0;
+	f = def;
+	return 1;
+}
+
+static unsigned makeFinite(RRVec2& v, const RRVec2& def)
+{
+	return makeFinite(v[0],def[0])+makeFinite(v[1],def[1]);
+}
+
+static unsigned makeFinite(RRVec3& v, const RRVec3& def)
+{
+	return makeFinite(v[0],def[0])+makeFinite(v[1],def[1])+makeFinite(v[2],def[2]);
+}
+
+unsigned RRCamera::fixInvalidValues()
+{
+	unsigned numFixes =
+		+ makeFinite(pos,RRVec3(0))
+		+ makeFinite(aspect,1)
+		+ makeFinite(fieldOfViewVerticalDeg,90)
+		+ makeFinite(anear,0.1f)
+		+ makeFinite(afar,100)
+		+ makeFinite(screenCenter,RRVec2(0))
+		+ makeFinite(orthoSize,100)
+		+ makeFinite(orthoSize,100)
+		+ makeFinite(yawPitchRollRad[0],0)
+		+ makeFinite(yawPitchRollRad[1],0)
+		+ makeFinite(yawPitchRollRad[2],0)
+		+ makeFinite(eyeSeparation,0.08f)
+		+ makeFinite(focalLength,0.5f)
+		+ makeFinite(dofNear,1)
+		+ makeFinite(dofFar,10);
+	if (numFixes)
+	{
+		updateView(true,true);
+		updateProjection();
+	}
+	return numFixes;
+}
+
+////////////////////////////////////////////////////////////////////////
+//
+// update
+
+void RRCamera::updateView(bool updateLightPos, bool updateLightDir)
+{
+	// pos, yawPitchRollRad -> viewMatrix, rrlight
+	// parameters are necessary because of following situation:
+	//	RRLight did change, we call updateAfterRRLightChanges()
+	//	in first step, updateAfterRRLightChanges() calls setPosition()
+	//	setPosition() calls updateView()
+	//	updateView() must not copy direction, because it would overwrite new value in RRLight by old value from RRCamera
+	RRMatrix3x4 view = RRMatrix3x4::rotationByYawPitchRoll(yawPitchRollRad);
+	for (unsigned i=0;i<4;i++)
+		for (unsigned j=0;j<4;j++)
+			viewMatrix[4*i+j] = (i<3) ? ((j<3)?view.m[i][j]:0) : ((j<3)?-pos.dot(view.getColumn(j)):1);
+	if (light && updateLightPos)
+		light->position = getPosition();
+	if (light && updateLightDir)
+		light->direction = getDirection();
+}
+
+void RRCamera::updateProjection()
+{
+	// orthoSize, aspect, screenCenter, anear, afar, fieldOfViewVerticalDeg -> projectionMatrix
+	if (orthogonal)
+	{
+		projectionMatrix[0] = 1/(orthoSize*aspect);
+		projectionMatrix[1] = 0;
+		projectionMatrix[2] = 0;
+		projectionMatrix[3] = 0;
+		projectionMatrix[4] = 0;
+		projectionMatrix[5] = 1/orthoSize;
+		projectionMatrix[6] = 0;
+		projectionMatrix[7] = 0;
+		projectionMatrix[8] = 0;
+		projectionMatrix[9] = 0;
+		projectionMatrix[10] = -1/(afar-anear);
+		projectionMatrix[11] = 0;
+		projectionMatrix[12] = -screenCenter.x;
+		projectionMatrix[13] = -screenCenter.y;
+		projectionMatrix[14] = -(anear+afar)/(afar-anear);
+		projectionMatrix[15] = 1;
+	}
+	else
+	{
+		projectionMatrix[0] = 1/(tan(RR_DEG2RAD(fieldOfViewVerticalDeg)/2)*aspect);
+		projectionMatrix[1] = 0;
+		projectionMatrix[2] = 0;
+		projectionMatrix[3] = 0;
+		projectionMatrix[4] = 0;
+		projectionMatrix[5] = 1/tan(RR_DEG2RAD(fieldOfViewVerticalDeg)/2);
+		projectionMatrix[6] = 0;
+		projectionMatrix[7] = 0;
+		projectionMatrix[8] = screenCenter.x;
+		projectionMatrix[9] = screenCenter.y;
+		projectionMatrix[10] = -(afar+anear)/(afar-anear);
+		projectionMatrix[11] = -1;
+		projectionMatrix[12] = 0;
+		projectionMatrix[13] = 0;
+		projectionMatrix[14] = -2*anear*afar/(afar-anear);
+		projectionMatrix[15] = 0;
+	}
+}
+
+}; // namespace
