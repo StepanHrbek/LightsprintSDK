2290d045cf2f669a4ee00b8c6f4042844252dfff
Stepan Hrbek <stepan.git@lightsprint.com>
2014-01-01 15:28:15 +0000

    svn6459
    sdk:
    -vs2005
    -vs2008 (nepodporuji std::type_index a opravovat novy renderer aby ho nepouzival mi nestoji za namahu)

--- build_SDK_win.bat
+++ build_SDK_win.bat
@@ -1,9 +1,5 @@
 @echo off
 
-start build_SDK_win_platform.bat Build vs2005 win32 "%VS80COMNTOOLS%\vsvars32.bat"
-start build_SDK_win_platform.bat Build vs2005 x64   "%vs80comntools%..\..\vc\bin\amd64\vcvarsamd64.bat"
-start build_SDK_win_platform.bat Build vs2008 win32 "%VS90COMNTOOLS%\vsvars32.bat"
-start build_SDK_win_platform.bat Build vs2008 x64   "%vs90comntools%..\..\vc\bin\amd64\vcvarsamd64.bat"
 start build_SDK_win_platform.bat Build vs2010 win32 "%VS100COMNTOOLS%\vsvars32.bat"
 start build_SDK_win_platform.bat Build vs2010 x64   "%vs100comntools%..\..\vc\bin\amd64\vcvars64.bat"
 start build_SDK_win_platform.bat Build vs2012 win32 "%VS110COMNTOOLS%\vsvars32.bat"
