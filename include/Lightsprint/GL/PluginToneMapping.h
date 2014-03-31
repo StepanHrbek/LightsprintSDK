142fe0ba75a3d2912b4c967ead691f209348aabf
Stepan Hrbek <stepan.git@lightsprint.com>
2014-03-31 12:14:04 +0000

    svn6702
    rr_gl, rr_ed:
    +PluginTonemapping aplikuje nastaveni z ToneParameters (saturace atd)

--- include/Lightsprint/GL/PluginToneMapping.h
+++ include/Lightsprint/GL/PluginToneMapping.h
@@ -1,6 +1,6 @@
 //----------------------------------------------------------------------------
 //! \file PluginToneMapping.h
-//! \brief LightsprintGL | tone mapping adjustment
+//! \brief LightsprintGL | tone mapping
 //! \author Copyright (C) Stepan Hrbek, Lightsprint 2008-2014
 //! All rights reserved
 //----------------------------------------------------------------------------
@@ -17,22 +17,15 @@ namespace rr_gl
 //
 // Tonemapping plugin
 
-//! Automatically adjusts tone mapping operator (brightness parameter) to simulate eye response in time.
-//!
-//! Only brightness parameter is adjusted here, image in render target does not change, it's your responsibility
-//! to pass brightness to renderer in next frame to achieve visible effect.
+//! Performs basic adjustments to output colors (brightness, contrast, saturation etc).
 class RR_GL_API PluginParamsToneMapping : public PluginParams
 {
 public:
-	//! Specified 'multiply screen by brightness' operator that will be adjusted.
-	rr::RRVec3& brightness;
-	//! Number of seconds since last call to this function.
-	rr::RRReal secondsSinceLastAdjustment;
-	//! Average pixel intensity we want to see after tone mapping, in 0..1 range, e.g. 0.5.
-	rr::RRReal targetIntensity;
+	//! Parameters that affect output colors.
+	const ToneParameters& tp;
 
 	//! Convenience ctor, for setting plugin parameters.
-	PluginParamsToneMapping(const PluginParams* _next, rr::RRVec3& _brightness, rr::RRReal _secondsSinceLastAdjustment, rr::RRReal _targetIntensity) : brightness(_brightness), secondsSinceLastAdjustment(_secondsSinceLastAdjustment), targetIntensity(_targetIntensity) {next=_next;}
+	PluginParamsToneMapping(const PluginParams* _next, const ToneParameters& _tp) : tp(_tp) {next=_next;}
 
 	//! Access to actual plugin code, called by Renderer.
 	virtual PluginRuntime* createRuntime(const rr::RRString& pathToShaders, const rr::RRString& pathToMaps) const;
--- src/LightsprintGL/PluginToneMapping.cpp
+++ src/LightsprintGL/PluginToneMapping.cpp
@@ -1,12 +1,9 @@
 // --------------------------------------------------------------------------
-// Tone mapping adjustment
+// Tone mapping
 // Copyright (C) 2008-2014 Stepan Hrbek, Lightsprint. All rights reserved.
 // --------------------------------------------------------------------------
 
 #include "Lightsprint/GL/PluginToneMapping.h"
-//#include "Lightsprint/GL/PreserveState.h"
-//#include <cstdlib>
-#include "Lightsprint/GL/FBO.h"
 
 namespace rr_gl
 {
@@ -17,28 +14,21 @@ namespace rr_gl
 
 class PluginRuntimeToneMapping : public PluginRuntime
 {
-	Texture* bigTexture;
-	Texture* smallTexture;
+	Texture* colorTexture;
 
 public:
 
 	PluginRuntimeToneMapping(const rr::RRString& pathToShaders, const rr::RRString& pathToMaps)
 	{
-		bigTexture = NULL;
-		smallTexture = NULL;
+		colorTexture = NULL;
 	}
 
 	virtual ~PluginRuntimeToneMapping()
 	{
-		if (smallTexture)
+		if (colorTexture)
 		{
-			delete smallTexture->getBuffer();
-			delete smallTexture;
-		}
-		if (bigTexture)
-		{
-			delete bigTexture->getBuffer();
-			delete bigTexture;
+			delete colorTexture->getBuffer();
+			delete colorTexture;
 		}
 	}
 
@@ -50,48 +40,10 @@ public:
 
 		if (!_renderer.getTextureRenderer()) return;
 
-		if (!bigTexture) bigTexture = new Texture(rr::RRBuffer::create(rr::BT_2D_TEXTURE,1,1,1,rr::BF_RGB,true,NULL),false,false,GL_NEAREST,GL_NEAREST,GL_REPEAT,GL_REPEAT);
-		bigTexture->bindTexture();
-		int bwidth = _sp.viewport[2];
-		int bheight = _sp.viewport[3];
-		glCopyTexImage2D(GL_TEXTURE_2D,0,GL_RGB,0,0,bwidth,bheight,0);
-
-		const unsigned swidth = 32;
-		const unsigned sheight = 32;
-		unsigned char buf[swidth*sheight*3];
-		unsigned histo[256];
-		unsigned avg = 0;
-		if (!smallTexture)
-		{
-			smallTexture = new Texture(rr::RRBuffer::create(rr::BT_2D_TEXTURE,swidth,sheight,1,rr::BF_RGB,true,NULL),false,false,GL_NEAREST,GL_NEAREST,GL_REPEAT,GL_REPEAT);
-		}
-		FBO oldFBOState = FBO::getState();
-		FBO::setRenderTarget(GL_COLOR_ATTACHMENT0,GL_TEXTURE_2D,smallTexture);
-		glViewport(0,0,swidth,sheight);
-		_renderer.getTextureRenderer()->render2D(bigTexture,NULL,1,0,0,1,1);
-		glReadPixels(0,0,swidth,sheight,GL_RGB,GL_UNSIGNED_BYTE,buf);
-		oldFBOState.restore();
-		glViewport(_sp.viewport[0],_sp.viewport[1],_sp.viewport[2],_sp.viewport[3]);
-		for (unsigned i=0;i<256;i++)
-			histo[i] = 0;
-		for (unsigned i=0;i<swidth*sheight*3;i+=3)
-			histo[RR_MAX3(buf[i],buf[i+1],buf[i+2])]++;
-		for (unsigned i=0;i<256;i++)
-			avg += histo[i]*i;
-		if (avg==0)
-		{
-			// completely black scene
-			//  extremely low brightness? -> reset it to 1
-			//  disabled lighting? -> avoid increasing brightness ad infinitum
-			pp.brightness = rr::RRVec3(1);
-		}
-		else
-		{
-			avg = avg/(swidth*sheight)+1;
-			if (histo[255]>=swidth*sheight*7/10) avg = 1000; // at least 70% of screen overshot, adjust faster
-			pp.brightness *= pow(pp.targetIntensity*255/avg,RR_CLAMPED(pp.secondsSinceLastAdjustment*0.15f,0.0002f,0.2f));
-		}
-		//rr::RRReporter::report(rr::INF1,"%d\n",avg);
+		if (!colorTexture) colorTexture = new Texture(rr::RRBuffer::create(rr::BT_2D_TEXTURE,1,1,1,rr::BF_RGB,true,NULL),false,false,GL_NEAREST,GL_NEAREST,GL_REPEAT,GL_REPEAT);
+		colorTexture->bindTexture();
+		glCopyTexImage2D(GL_TEXTURE_2D,0,GL_RGB,_sp.viewport[0],_sp.viewport[1],_sp.viewport[2],_sp.viewport[3],0);
+		_renderer.getTextureRenderer()->render2D(colorTexture,&pp.tp,0,0,1,1);
 	}
 };
 
