364f1284ff4879154971667edaebf4c9771fcd47
Stepan Hrbek <stepan.git@lightsprint.com>
2014-05-04 11:20:53 +0000

    svn6773
    doc, whitespace

--- include/Lightsprint/GL/PluginPanorama.h
+++ include/Lightsprint/GL/PluginPanorama.h
@@ -18,7 +18,7 @@ enum PanoramaMode
 	PM_OFF              =0, ///< common non-panorama mode
 	PM_EQUIRECTANGULAR  =1, ///< 360 degree render in equirectangular projection \image html 360-equirect.jpg
 	PM_LITTLE_PLANET    =2, ///< 360 degree render in stereographic (little planet) projection \image html 360-planet.jpg
-	PM_FISHEYE             =3, ///< variable FOV fisheye render; suitable for dome projection with spherical mirror in center \image html 180-dome.jpg
+	PM_FISHEYE          =3, ///< variable FOV fisheye render; suitable for dome projection with spherical mirror in center \image html 180-dome.jpg
 };
 
 enum PanoramaCoverage
