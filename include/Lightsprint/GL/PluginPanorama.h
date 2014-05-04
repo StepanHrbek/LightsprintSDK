33e409acb5ec17842401f15edfa321f4b60a2c37
Stepan Hrbek <stepan.git@lightsprint.com>
2014-05-04 12:38:17 +0000

    svn6774
    doc

--- include/Lightsprint/GL/PluginPanorama.h
+++ include/Lightsprint/GL/PluginPanorama.h
@@ -16,17 +16,17 @@ namespace rr_gl
 enum PanoramaMode
 {
 	PM_OFF              =0, ///< common non-panorama mode
-	PM_EQUIRECTANGULAR  =1, ///< 360 degree render in equirectangular projection \image html 360-equirect.jpg
-	PM_LITTLE_PLANET    =2, ///< 360 degree render in stereographic (little planet) projection \image html 360-planet.jpg
-	PM_FISHEYE          =3, ///< variable FOV fisheye render; suitable for dome projection with spherical mirror in center \image html 180-dome.jpg
+	PM_EQUIRECTANGULAR  =1, ///< 360 degree render in equirectangular projection \image html pano-equirect.jpg
+	PM_LITTLE_PLANET    =2, ///< 360 degree render in stereographic (little planet) projection \image html pano-littleplanet.jpg
+	PM_FISHEYE          =3, ///< variable FOV fisheye render; suitable for dome projection with spherical mirror in center \image html pano-fisheye.jpg
 };
 
 enum PanoramaCoverage
 {
-	PC_FULL_STRETCH     =0, ///< panorama covers whole output area
-	PC_FULL             =1, ///< panorama covers as large output area as possible while preserving aspect ratio
-	PC_TRUNCATE_BOTTOM  =2, ///< panorama covers full output width, going from top down, with aspect preserved. bottom might be cropped or empty, depending on output size
-	PC_TRUNCATE_TOP     =3, ///< panorama covers full output width, going from bottom up, with aspect preserved. upper part might be cropped or empty, depending on output size
+	PC_FULL_STRETCH     =0, ///< panorama covers whole output area \image html cover-stretch.jpg
+	PC_FULL             =1, ///< panorama covers as large output area as possible while preserving aspect ratio \image html cover-full.jpg
+	PC_TRUNCATE_BOTTOM  =2, ///< panorama covers full output width, going from top down, with aspect preserved. bottom might be cropped or empty, depending on output size \image html cover-trunc-bottom.jpg
+	PC_TRUNCATE_TOP     =3, ///< panorama covers full output width, going from bottom up, with aspect preserved. upper part might be cropped or empty, depending on output size \image html cover-trunc-top.jpg
 };
 
 /////////////////////////////////////////////////////////////////////////////
@@ -36,11 +36,11 @@ enum PanoramaCoverage
 //! Renders scene (calls next plugin) into cubemap, then transforms it back to 2d.
 //
 //! <table border=0><tr align=top><td>
-//! \image html 360-equirect.jpg
+//! \image html pano-equirect.jpg
 //! </td><td>
-//! \image html 360-planet.jpg
+//! \image html pano-littleplanet.jpg
 //! </td><td>
-//! \image html 180-dome.jpg
+//! \image html pano-fisheye.jpg
 //! </td></tr></table>
 class RR_GL_API PluginParamsPanorama : public PluginParams
 {
