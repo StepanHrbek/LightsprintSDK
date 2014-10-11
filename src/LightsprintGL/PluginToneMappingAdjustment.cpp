92bd02b20343ca6c753e17200b4a4d2be1f9f32d
Stepan Hrbek <stepan.git@lightsprint.com>
2014-10-11 09:49:00 +0000

    svn6977
    rr_gl:
    *PluginTonemappingAdjustment pouziva PBO
     +rychlejsi, CPU nemusi tolik cekat na GPU

--- src/LightsprintGL/PluginToneMappingAdjustment.cpp
+++ src/LightsprintGL/PluginToneMappingAdjustment.cpp
@@ -8,6 +8,11 @@
 //#include <cstdlib>
 #include "Lightsprint/GL/FBO.h"
 
+#define PBO // not present in core OpenGL ES 2.0
+#define SMALL_W 32
+#define SMALL_H 32
+#define SMALL_E 3 // 3 for RGB, 4 for RGBA
+
 namespace rr_gl
 {
 
@@ -19,6 +24,10 @@ class PluginRuntimeToneMappingAdjustment : public PluginRuntime
 {
 	Texture* bigTexture;
 	Texture* smallTexture;
+#ifdef PBO
+	GLuint pbo[2];
+	unsigned pboIndex;
+#endif
 
 public:
 
@@ -26,10 +35,22 @@ public:
 	{
 		bigTexture = NULL;
 		smallTexture = NULL;
+#ifdef PBO
+		glGenBuffers(2,pbo);
+		glBindBufferARB(GL_PIXEL_PACK_BUFFER_ARB, pbo[0]);
+		glBufferDataARB(GL_PIXEL_PACK_BUFFER_ARB, SMALL_W*SMALL_H*SMALL_E, 0, GL_STREAM_READ_ARB);
+		glBindBufferARB(GL_PIXEL_PACK_BUFFER_ARB, pbo[1]);
+		glBufferDataARB(GL_PIXEL_PACK_BUFFER_ARB, SMALL_W*SMALL_H*SMALL_E, 0, GL_STREAM_READ_ARB);
+		glBindBufferARB(GL_PIXEL_PACK_BUFFER_ARB, 0);
+		pboIndex = 0;
+#endif
 	}
 
 	virtual ~PluginRuntimeToneMappingAdjustment()
 	{
+#ifdef PBO
+		glDeleteBuffers(2,pbo);
+#endif
 		if (smallTexture)
 		{
 			delete smallTexture->getBuffer();
@@ -54,25 +75,37 @@ public:
 		bigTexture->bindTexture();
 		glCopyTexImage2D(GL_TEXTURE_2D,0,GL_RGB,_sp.viewport[0],_sp.viewport[1],_sp.viewport[2],_sp.viewport[3],0);
 
-		const unsigned swidth = 32;
-		const unsigned sheight = 32;
-		unsigned char buf[swidth*sheight*3];
+#ifndef PBO
+		unsigned char buf[SMALL_W*SMALL_H*SMALL_E];
+#endif
 		unsigned histo[256];
 		unsigned avg = 0;
 		if (!smallTexture)
 		{
-			smallTexture = new Texture(rr::RRBuffer::create(rr::BT_2D_TEXTURE,swidth,sheight,1,rr::BF_RGB,true,NULL),false,false,GL_NEAREST,GL_NEAREST,GL_REPEAT,GL_REPEAT);
+			smallTexture = new Texture(rr::RRBuffer::create(rr::BT_2D_TEXTURE,SMALL_W,SMALL_H,1,rr::BF_RGB,true,NULL),false,false,GL_NEAREST,GL_NEAREST,GL_REPEAT,GL_REPEAT);
 		}
 		FBO oldFBOState = FBO::getState();
 		FBO::setRenderTarget(GL_COLOR_ATTACHMENT0,GL_TEXTURE_2D,smallTexture,oldFBOState);
-		glViewport(0,0,swidth,sheight);
+		glViewport(0,0,SMALL_W,SMALL_H);
 		_renderer.getTextureRenderer()->render2D(bigTexture,NULL,0,0,1,1);
-		glReadPixels(0,0,swidth,sheight,GL_RGB,GL_UNSIGNED_BYTE,buf);
+#ifdef PBO
+		glBindBuffer(GL_PIXEL_PACK_BUFFER,pbo[pboIndex]);
+		glReadPixels(0,0,SMALL_W,SMALL_H,GL_RGB,GL_UNSIGNED_BYTE,0);
+#else
+		glReadPixels(0,0,SMALL_W,SMALL_H,GL_RGB,GL_UNSIGNED_BYTE,buf);
+#endif
+
 		oldFBOState.restore();
 		glViewport(_sp.viewport[0],_sp.viewport[1],_sp.viewport[2],_sp.viewport[3]);
+#ifdef PBO
+		glBindBuffer(GL_PIXEL_PACK_BUFFER,pbo[1-pboIndex]);
+		GLubyte* buf = (GLubyte*)glMapBuffer(GL_PIXEL_PACK_BUFFER,GL_READ_ONLY);
+		if(buf)
+		{
+#endif
 		for (unsigned i=0;i<256;i++)
 			histo[i] = 0;
-		for (unsigned i=0;i<swidth*sheight*3;i+=3)
+		for (unsigned i=0;i<SMALL_W*SMALL_H*SMALL_E;i+=SMALL_E)
 			histo[RR_MAX3(buf[i],buf[i+1],buf[i+2])]++;
 		for (unsigned i=0;i<256;i++)
 			avg += histo[i]*i;
@@ -85,10 +118,16 @@ public:
 		}
 		else
 		{
-			avg = avg/(swidth*sheight)+1;
-			if (histo[255]>=swidth*sheight*7/10) avg = 1000; // at least 70% of screen overshot, adjust faster
+			avg = avg/(SMALL_W*SMALL_H)+1;
+			if (histo[255]>=SMALL_W*SMALL_H*7/10) avg = 1000; // at least 70% of screen overshot, adjust faster
 			pp.brightness *= pow(pp.targetIntensity*255/avg,RR_CLAMPED(pp.secondsSinceLastAdjustment*0.15f,0.0002f,0.2f));
 		}
+#ifdef PBO
+		glUnmapBuffer(GL_PIXEL_PACK_BUFFER);
+		}
+		pboIndex = 1-pboIndex;
+		glBindBuffer(GL_PIXEL_PACK_BUFFER,0);
+#endif
 		//rr::RRReporter::report(rr::INF1,"%d\n",avg);
 	}
 };
