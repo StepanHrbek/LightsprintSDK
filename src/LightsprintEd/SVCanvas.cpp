dcc838b0143e50f13277af08673ce9cb0ea1f78d
Stepan Hrbek <stepan.git@lightsprint.com>
2014-12-18 18:00:30 +0000

    svn7161
    rr_ed:
    @v ~SVCanvas jsem neshazoval oculus renderer, takze v pristim SVCanvas (po File/Open) nefungoval

--- src/LightsprintEd/SVCanvas.cpp
+++ src/LightsprintEd/SVCanvas.cpp
@@ -552,6 +552,8 @@ SVCanvas::~SVCanvas()
 
 #ifdef SUPPORT_OCULUS
 	// oculus
+	rr::RRReporter::report(rr::INF2,"0\n");
+	ovrHmd_ConfigureRendering(svframe->oculusHMD, NULL, 0, NULL, NULL);
 	rr::RRReporter::report(rr::INF2,"1\n");
 	for (unsigned i=0;i<2;i++)
 		if (oculusTexture[i])
