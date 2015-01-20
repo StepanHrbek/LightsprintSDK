7095bf7f3d9aca58d650ffcbf5516850c6f8a54c
Stepan Hrbek <stepan.git@lightsprint.com>
2015-01-20 23:26:36 +0000

    svn7291
    osx: @err

--- src/LightsprintIO/ImportLightsprint/RRSerialization.h
+++ src/LightsprintIO/ImportLightsprint/RRSerialization.h
@@ -842,7 +842,8 @@ void save(Archive & ar, const rr::RRObject& a, const unsigned int version)
 {
 	ar & make_nvp("name",a.name);
 	ar & make_nvp("faceGroups",a.faceGroups);
-	ar & make_nvp("worldMatrix",RRMatrix3x4(a.getWorldMatrixRef()));
+	rr::RRMatrix3x4 matrix(a.getWorldMatrixRef());
+	ar & make_nvp("worldMatrix",matrix);
 	ar & make_nvp("isDynamic",a.isDynamic);
 	ar & make_nvp("enabled",a.enabled);
 	{
