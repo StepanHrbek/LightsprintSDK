ed9c4fbc838e7a0edfadf2cdd25c3fc272e78e4c
Stepan Hrbek <stepan.git@lightsprint.com>
2015-05-07 19:12:44 +0200

    Remove ImageMagick, no longer needed.

--- doc/doxygen/fix_png_opacity.bat
+++
@@ -1,8 +0,0 @@
-"..\..\3rd\ImageMagick\convert.exe" -quality 100 -transparent #ffffff -trim libraries.png libraries.png
-"..\..\3rd\ImageMagick\convert.exe" -quality 100 -opaque #ffffff Integration1a.png Integration1a.png
-"..\..\3rd\ImageMagick\convert.exe" -quality 100 -opaque #ffffff Integration1b.png Integration1b.png
-"..\..\3rd\ImageMagick\convert.exe" -quality 100 -opaque #ffffff Integration1c.png Integration1c.png
-"..\..\3rd\ImageMagick\convert.exe" -quality 100 -opaque #ffffff Integration2.png Integration2.png
-"..\..\3rd\ImageMagick\convert.exe" -quality 100 -opaque #ffffff Integration3.png Integration3.png
-"..\..\3rd\ImageMagick\convert.exe" -quality 100 -opaque #ffffff Integration4a.png Integration4a.png
-"..\..\3rd\ImageMagick\convert.exe" -quality 100 -opaque #ffffff Integration4b.png Integration4b.png
