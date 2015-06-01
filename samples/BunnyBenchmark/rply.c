c81edd7faa326ba49941c3325f3827ebd52aefdb
Stepan Hrbek <stepan.git@lightsprint.com>
2015-06-01 22:25:10 +0200

    Fix: BunnyBenchmark broken since switch to git.
    When I installed git and then converted repo from svn to git, I had to answer some confusing questions about CR/LF. I left defaults. Today I found first consequence. Git silently converted BunnyBenchmark .ply from CR to CRLF format and BunnyBenchmark stopped working.
    I fixed it by changing fopen() from "rb" to "rt".

--- samples/BunnyBenchmark/rply.c
+++ samples/BunnyBenchmark/rply.c
@@ -294,7 +294,7 @@ p_ply ply_open(const char *name, p_ply_error_cb error_cb) {
         return NULL;
     }
     assert(name);
-    fp = fopen(name, "rb");
+    fp = fopen(name, "rt");
     if (!fp) {
         error_cb("Unable to open file");
         return NULL;
