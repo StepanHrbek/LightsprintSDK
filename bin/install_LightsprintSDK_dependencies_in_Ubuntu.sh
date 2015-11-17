acb36cfedd1087d6f85339fd16775a6bd4c9ec1e
Stepan Hrbek <stepan.git@lightsprint.com>
2015-11-17 17:55:19 +0100

    Linux: update dependencies script.

--- bin/install_LightsprintSDK_dependencies_in_Ubuntu.sh
+++ bin/install_LightsprintSDK_dependencies_in_Ubuntu.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 
-# Install Lightsprint SDK dependencies in Ubuntu 10.10.
-sudo apt-get install g++ libboost-dev libboost-serialization-dev libboost-filesystem-dev libboost-iostreams-dev libglew-dev libfreeimage-dev libglut3-dev
+# Install Lightsprint SDK dependencies in Debian (most likely works also in Ubuntu)
+sudo apt-get install g++ make libboost-dev libboost-serialization-dev libboost-filesystem-dev libboost-iostreams-dev libglew-dev libfreeimage-dev freeglut3-dev
