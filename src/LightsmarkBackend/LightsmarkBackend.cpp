9563158c80bcf579fdadc3f2d7d6b638fafb95b1
Stepan Hrbek <stepan.git@lightsprint.com>
2015-12-07 00:44:14 +0100

    LM: Disable fps percentiles, to improve fps.

--- src/LightsmarkBackend/LightsmarkBackend.cpp
+++ src/LightsmarkBackend/LightsmarkBackend.cpp
@@ -326,7 +326,7 @@ void renderScene(rr_gl::UberProgramSetup uberProgramSetup, unsigned firstInstanc
 	ppScene.layerLightmap = LAYER_LIGHTMAPS;
 	ppScene.layerEnvironment = LAYER_ENVIRONMENT;
 	ppScene.layerLDM = uberProgramSetup.LIGHT_INDIRECT_DETAIL_MAP ? level->getLDMLayer() : UINT_MAX;
-	rr_gl::PluginParamsFPS ppFPS(&ppScene,g_fpsCounter,true);
+	rr_gl::PluginParamsFPS ppFPS(&ppScene,g_fpsCounter,!true);
 	rr_gl::PluginParamsShared ppShared;
 	ppShared.camera = &camera;
 	ppShared.viewport[2] = winWidth;
