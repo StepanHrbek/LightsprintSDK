6bfd4b42f537f214295c1a2721bfc845f9f7f9d4
Stepan Hrbek <stepan.git@lightsprint.com>
2016-02-03 02:24:37 +0100

    Add vs2015 sections to clean/build scripts.

--- build_SDK_win.bat
+++ build_SDK_win.bat
@@ -6,3 +6,5 @@ start build_SDK_win_platform.bat Build vs2012 win32 "%VS110COMNTOOLS%\vsvars32.b
 start build_SDK_win_platform.bat Build vs2012 x64   "%vs110comntools%..\..\vc\bin\x86_amd64\vcvarsx86_amd64.bat"
 start build_SDK_win_platform.bat Build vs2013 win32 "%VS120COMNTOOLS%\vsvars32.bat"
 start build_SDK_win_platform.bat Build vs2013 x64   "%vs120comntools%..\..\vc\bin\x86_amd64\vcvarsx86_amd64.bat"
+start build_SDK_win_platform.bat Build vs2015 win32 "%VS140COMNTOOLS%\vsvars32.bat"
+start build_SDK_win_platform.bat Build vs2015 x64   "%vs140comntools%..\..\vc\bin\x86_amd64\vcvarsx86_amd64.bat"
