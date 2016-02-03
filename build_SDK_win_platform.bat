638b5255a817cba88a842b173c1669154fe0a073
Stepan Hrbek <stepan.git@lightsprint.com>
2016-02-03 02:23:34 +0100

    When using msbuild, expand environment variables in .sln.
    Msbuild does not understand env.variables, expansion is necessary.
    devenv.exe understands variables, but I have only devenv 2010/13/15, I don't have 2012.
    Env.variables are used to include external Oculus project in Lightsprint solution.

--- build_SDK_win_platform.bat
+++ build_SDK_win_platform.bat
@@ -9,22 +9,25 @@ set ERR=%ACTION%_SDK_win_%COMPILER%_%TARGET%.err
 set TARGET2=^|%TARGET%
 call %SETENVBAT% >nul
 
+rem msbuild and devenv are nearly equal here. But only devenv accepts %ENV_VARIABLE% in sln, msbuild needs us to expand them.
+rem Call devenv rather than devenv.exe, it's necessary for reading stdout and exitcode.
+rem Using one or the other is quite arbitrary, depends on tools one has installed, you can change it.
+
+rem Expand environment variables in sln.
+powershell.exe -command "(Get-Content 'src\Lightsprint.%COMPILER%.sln' ) | Foreach-Object { [Environment]::ExpandEnvironmentVariables($_) } | Set-Content 'src\Lightsprint.%COMPILER%.%TARGET%.sln'"
+
 echo %ACTION%ing %COMPILER% "Debug DLL%TARGET2%"...
-rem Nesmim volat primo devenv.exe, nedostal bych z nej stdout a exitcode.
-if     %COMPILER%==vs2010 devenv  src\Lightsprint.%COMPILER%.sln /%ACTION% "Debug DLL%TARGET2%" >%ERR%
-if NOT %COMPILER%==vs2010 msbuild src\Lightsprint.%COMPILER%.sln /t:%ACTION% /p:Configuration="Debug DLL" /p:Platform=%TARGET% >%ERR%
+if     %COMPILER%==vs2010 devenv  src\Lightsprint.%COMPILER%.%TARGET%.sln /%ACTION% "Debug DLL%TARGET2%" >%ERR%
+if NOT %COMPILER%==vs2010 msbuild src\Lightsprint.%COMPILER%.%TARGET%.sln /t:%ACTION% /p:Configuration="Debug DLL" /p:Platform=%TARGET% >%ERR%
 if ERRORLEVEL 1 goto error
 
 echo %ACTION%ing %COMPILER% "Release DLL%TARGET2%"...
-if     %COMPILER%==vs2010 devenv  src\Lightsprint.%COMPILER%.sln /%ACTION% "Release DLL%TARGET2%" >%ERR%
-if NOT %COMPILER%==vs2010 msbuild src\Lightsprint.%COMPILER%.sln /t:%ACTION% /p:Configuration="Release DLL" /p:Platform=%TARGET% >%ERR%
+if     %COMPILER%==vs2010 devenv  src\Lightsprint.%COMPILER%.%TARGET%.sln /%ACTION% "Release DLL%TARGET2%" >%ERR%
+if NOT %COMPILER%==vs2010 msbuild src\Lightsprint.%COMPILER%.%TARGET%.sln /t:%ACTION% /p:Configuration="Release DLL" /p:Platform=%TARGET% >%ERR%
 if ERRORLEVEL 1 goto error
 
-if %COMPILER%==vs2008 if %TARGET%==win32 (
-	echo %ACTION%ing %COMPILER% "Shipping DLL%TARGET2%"...
-	devenv src\RR.%COMPILER%.sln /%ACTION% "Shipping DLL%TARGET2%"" >%ERR%
-	if ERRORLEVEL 1 goto error
-)
+rem Cleanup.
+del "src\Lightsprint.%COMPILER%.%TARGET%.sln"
 
 del %ERR%
 echo OK, done.
