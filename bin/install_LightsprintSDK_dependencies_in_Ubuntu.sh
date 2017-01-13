0b372a2afedc9b86d1b54b609d7811231401c6e1
Stepan Hrbek <stepan.git@lightsprint.com>
2017-01-14 00:38:18 +0100

    Linux: Update script that installs dependencies.

--- bin/install_LightsprintSDK_dependencies_in_Ubuntu.sh
+++ bin/install_LightsprintSDK_dependencies_in_Ubuntu.sh
@@ -1,22 +1,23 @@
 #/bin/sh
 
-# Install Lightsprint SDK development dependencies in Debian (most likely works also in Ubuntu)
+# Run this as root
+# to install Lightsprint SDK development dependencies in Debian (most likely works also in Ubuntu)
 # In case that you need only binary dependencies, try removing all -dev.
 
 # for build system
-sudo apt-get install g++ make
+apt install g++ make
 
 # for LightsprintCore
-sudo apt-get install libboost-dev libboost-filesystem-dev libboost-regex-dev
+apt install libboost-dev libboost-filesystem-dev libboost-regex-dev
 
 # for LightsprintGL
-sudo apt-get install libglew-dev
+apt install libglew-dev
 
 # for LightsprintIO
-sudo apt-get install libboost-serialization-dev libboost-iostreams-dev libboost-locale-dev libboost-thread-dev libfreeimage-dev libassimp-dev portaudio19-dev libavdevice-dev libavfilter-dev
+apt install libboost-serialization-dev libboost-iostreams-dev libboost-locale-dev libboost-thread-dev libfreeimage-dev libassimp-dev portaudio19-dev libavdevice-dev libavfilter-dev
 
 # for LightsprintEd
-sudo apt-get install libwxgtk3.0-dev
+apt install libwxgtk3.0-dev
 
 # for samples
-sudo apt-get install freeglut3-dev
+apt install freeglut3-dev
