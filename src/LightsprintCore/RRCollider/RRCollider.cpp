ad2be0b6d4920e97b2881447e174b7c882cc05fc
Stepan Hrbek <stepan.git@lightsprint.com>
2018-05-13 21:07:19 +0200

    Update Embree 2.x -> 3.1.
    Only windows x64 binary is included for now, the rest is old (osx) or missing (win32, linux).

--- src/LightsprintCore/RRCollider/RRCollider.cpp
+++ src/LightsprintCore/RRCollider/RRCollider.cpp
@@ -19,9 +19,9 @@
 	#include <excpt.h> // EXCEPTION_EXECUTE_HANDLER
 #endif
 #ifdef SUPPORT_EMBREE
-	#include "embree2/rtcore.h"
-	#include "embree2/rtcore_ray.h"
-	#pragma comment(lib,"embree.lib")
+	#include "embree3/rtcore.h"
+	#include "embree3/rtcore_ray.h"
+	#pragma comment(lib,"embree3.lib")
 #endif
 
 namespace rr
@@ -34,6 +34,13 @@ namespace rr
 // Embree
 
 static unsigned s_numEmbreeColliders = 0;
+static RTCDevice s_embreeDevice = nullptr;
+
+// as a ray context, we pass this extended structure
+struct RTCIntersectContextEx : public RTCIntersectContext
+{
+	RRRay* rrRay;
+};
 
 class EmbreeCollider : public RRCollider
 {
@@ -41,26 +48,22 @@ class EmbreeCollider : public RRCollider
 	RTCScene rtcScene;
 	RRCollider::IntersectTechnique technique;
 
-	struct Ray : public RTCRay
+	struct Ray : public RTCRayHit
 	{
 		RRRay& rrRay;
 		Ray(RRRay& _rrRay) : rrRay(_rrRay) {}
 	};
 
-	template <class C, class D>
-	static void copyVec3(const C& src, D& dst)
-	{
-		dst[0] = src[0];
-		dst[1] = src[1];
-		dst[2] = src[2];
-	}
-
 	static void copyRrRayToRtc(const RRRay& rrRay, RTCRay& rtcRay)
 	{
-		copyVec3(rrRay.rayDir,rtcRay.dir);
+		rtcRay.dir_x = rrRay.rayDir.x;
+		rtcRay.dir_y = rrRay.rayDir.y;
+		rtcRay.dir_z = rrRay.rayDir.z;
 		if (rrRay.rayLengthMin>=0)
 		{
-			copyVec3(rrRay.rayOrigin,rtcRay.org);
+			rtcRay.org_x = rrRay.rayOrigin.x;
+			rtcRay.org_y = rrRay.rayOrigin.y;
+			rtcRay.org_z = rrRay.rayOrigin.z;
 			rtcRay.tnear = rrRay.rayLengthMin;
 			rtcRay.tfar = rrRay.rayLengthMax;
 		}
@@ -68,26 +71,27 @@ class EmbreeCollider : public RRCollider
 		{
 			// our rayLengthMin is usually negative for orthogonal cameras
 			// embree would clamp tnear to 0, we need to prevent that
-			copyVec3(rrRay.rayOrigin+rrRay.rayDir*rrRay.rayLengthMin,rtcRay.org);
+			rtcRay.org_x = rrRay.rayOrigin.x+rrRay.rayDir.x*rrRay.rayLengthMin;
+			rtcRay.org_y = rrRay.rayOrigin.y+rrRay.rayDir.y*rrRay.rayLengthMin;
+			rtcRay.org_z = rrRay.rayOrigin.z+rrRay.rayDir.z*rrRay.rayLengthMin;
 			rtcRay.tnear = 0;
 			rtcRay.tfar = rrRay.rayLengthMax-rrRay.rayLengthMin;
 		}
-		rtcRay.geomID = RTC_INVALID_GEOMETRY_ID;
-		rtcRay.primID = RTC_INVALID_GEOMETRY_ID;
-		rtcRay.instID = RTC_INVALID_GEOMETRY_ID;
-		rtcRay.mask = 0xffffffff;
 		rtcRay.time = 0;
+		rtcRay.mask = 0xffffffff;
+		rtcRay.id = 0; // not used
+		rtcRay.flags = 0; // reserved, must be 0
 	}
 
-	static void copyRtcHitToRr(const RTCRay& rtcRay, RRRay& rrRay)
+	static void copyRtcHitToRr(const RTCRayHit& rtcRay, RRRay& rrRay)
 	{
-		float rtcRay_tfar_fixed = (rrRay.rayLengthMin>=0) ? rtcRay.tfar : (rtcRay.tfar+rrRay.rayLengthMin);
+		float rtcRay_tfar_fixed = (rrRay.rayLengthMin>=0) ? rtcRay.ray.tfar : (rtcRay.ray.tfar+rrRay.rayLengthMin);
 		if (rrRay.rayFlags&RRRay::FILL_DISTANCE)
-			rrRay.hitDistance = rtcRay_tfar_fixed;
+			rrRay.hitDistance = rtcRay_tfar_fixed; // in callback: "The hit distance is provided as the tfar value of the ray." rtcIntersect1: "When an intersection is found, the hit distance is written into the tfar member of the ray"
 		if (rrRay.rayFlags&RRRay::FILL_TRIANGLE)
-			rrRay.hitTriangle = rtcRay.primID;
+			rrRay.hitTriangle = rtcRay.hit.primID;
 		if (rrRay.rayFlags&RRRay::FILL_POINT2D)
-			rrRay.hitPoint2d = RRVec2(rtcRay.u,rtcRay.v);
+			rrRay.hitPoint2d = RRVec2(rtcRay.hit.u,rtcRay.hit.v);
 		if (rrRay.rayFlags&RRRay::FILL_POINT3D)
 			rrRay.hitPoint3d = rrRay.rayOrigin + rrRay.rayDir*rtcRay_tfar_fixed;
 		if (rrRay.rayFlags&RRRay::FILL_PLANE)
@@ -95,7 +99,7 @@ class EmbreeCollider : public RRCollider
 			// embree Ng is unnormalized, goes from back side
 			// Lightsprint RRVec3(hitPlane) is normalized, goes from front side
 			RRVec3 hitPoint3d = rrRay.rayOrigin + rrRay.rayDir*rtcRay_tfar_fixed;
-			RRVec3 n(-rtcRay.Ng[0],-rtcRay.Ng[1],-rtcRay.Ng[2]);
+			RRVec3 n(rtcRay.hit.Ng_x,rtcRay.hit.Ng_y,rtcRay.hit.Ng_z);
 			float siz = n.length();
 			rrRay.hitPlane[0] = n.x/siz;
 			rrRay.hitPlane[1] = n.y/siz;
@@ -104,52 +108,127 @@ class EmbreeCollider : public RRCollider
 		}
 		if (rrRay.rayFlags&RRRay::FILL_SIDE)
 		{
-			RRVec3 n(-rtcRay.Ng[0],-rtcRay.Ng[1],-rtcRay.Ng[2]);
+			RRVec3 n(rtcRay.hit.Ng_x,rtcRay.hit.Ng_y,rtcRay.hit.Ng_z);
 			rrRay.hitFrontSide = rrRay.rayDir.dot(n.normalized())<0;
 		}
 	}
 
-	static void callback(void* _userPtr, RTCRay& _rtcRay)
+	// Embree's version of Lightsprint's collision handler
+	static void intersectFilter(const struct RTCFilterFunctionNArguments* _args)
 	{
-		Ray& rtcRay = *(Ray*)&_rtcRay;
-
-		// copy rtcRay to rrRay
-		copyRtcHitToRr(rtcRay,rtcRay.rrRay);
-
-		if (rtcRay.rrRay.collisionHandler)
+		RTCIntersectContextEx* contextEx = (RTCIntersectContextEx*)_args->context;
+		RRRay* rrRayGlobal = contextEx->rrRay;
+		if (rrRayGlobal->collisionHandler)
 		{
-			if (!rtcRay.rrRay.collisionHandler->collides(rtcRay.rrRay))
-				rtcRay.geomID = RTC_INVALID_GEOMETRY_ID;
+			if(_args->N!=1)
+			{
+				// This function should work for all packet sizes, but as we expect only size 1, we tell compiler to hardcode size=1.
+				// If we later work with packets, we just remove this if.
+				RRReporter::report(ERRO,"Unexpected ray packet.\n");
+				return;
+			}
+			for (unsigned i=0;i<_args->N;i++)
+			{
+				//const RTCRay* ray = _args->ray[_args->N];
+				//const RTCHit* hit = _args->hit[_args->N];
+				//const RRRay* rrRay = (*(Ray*)&_args->geometryUserPtr)->rrRay;
+				//Ray& rtcRay = *(Ray*)&_args->geometryUserPtr;
+				// a) copy embree data into temporary rr ray, so we can call rr callback
+				//RRRay rrRay; // possibly unaligned, would be bad for intersections, does not matter for callback
+				//rrRay.collisionHandler = ?;
+				//_args->geometryUserPtr
+				//_args->context->filter
+
+#if 0
+				// shorter source code but slower
+				RRRay rrRayLocal(*rrRayGlobal); // create local copy of original ray. possibly unaligned, would be bad for intersections, does not matter for callback
+				RTCRayHit rtcRayHit;
+				rtcRayHit.ray = rtcGetRayFromRayN(_args->ray,_args->N,i);
+				rtcRayHit.hit = rtcGetHitFromHitN(_args->hit,_args->N,i);
+				copyRtcHitToRr(rtcRayHit,rrRayLocal); // update local copy of hit from embree
+#else
+				RRRay rrRayLocal; // possibly unaligned, would be bad for intersections, does not matter for callback
+				rrRayLocal.rayOrigin = rrRayGlobal->rayOrigin; // =
+				//rrRayLocal.rayOrigin.x = RTCRayN_org_x(_args->ray,_args->N,i);
+				//rrRayLocal.rayOrigin.y = RTCRayN_org_y(_args->ray,_args->N,i);
+				//rrRayLocal.rayOrigin.z = RTCRayN_org_z(_args->ray,_args->N,i);
+				rrRayLocal.rayDir = rrRayGlobal->rayDir; // =
+				//rrRayLocal.rayDir.x = RTCRayN_dir_x(_args->ray,_args->N,i);
+				//rrRayLocal.rayDir.y = RTCRayN_dir_y(_args->ray,_args->N,i);
+				//rrRayLocal.rayDir.z = RTCRayN_dir_z(_args->ray,_args->N,i);
+				rrRayLocal.rayLengthMin = rrRayGlobal->rayLengthMin; // = RTCRayN_tnear(_args->ray,_args->N,i)
+				rrRayLocal.rayLengthMax = rrRayGlobal->rayLengthMax; // embree forgets original value, RTCRayN_tfar(_args->ray,_args->N,i) contains hitDistance
+				rrRayLocal.rayFlags = rrRayGlobal->rayFlags;
+				rrRayLocal.hitDistance = RTCRayN_tfar(_args->ray,_args->N,i); // in callback: "The hit distance is provided as the tfar value of the ray."
+				rrRayLocal.hitTriangle = RTCHitN_primID(_args->hit,_args->N,i);
+				rrRayLocal.hitPoint2d.x = RTCHitN_u(_args->hit,_args->N,i);
+				rrRayLocal.hitPoint2d.y = RTCHitN_v(_args->hit,_args->N,i);
+				rrRayLocal.hitPoint3d = rrRayLocal.rayOrigin + rrRayLocal.rayDir*rrRayLocal.hitDistance;
+				{
+					// embree Ng is unnormalized, goes from front side
+					// Lightsprint RRVec3(hitPlane) is normalized, goes from front side
+					RRVec3 n(RTCHitN_Ng_x(_args->hit,_args->N,i),RTCHitN_Ng_y(_args->hit,_args->N,i),RTCHitN_Ng_z(_args->hit,_args->N,i));
+					float siz = n.length();
+					rrRayLocal.hitPlane[0] = n.x/siz;
+					rrRayLocal.hitPlane[1] = n.y/siz;
+					rrRayLocal.hitPlane[2] = n.z/siz;
+					rrRayLocal.hitPlane[3] = -(rrRayLocal.hitPoint3d[0] * rrRayLocal.hitPlane[0] + rrRayLocal.hitPoint3d[1] * rrRayLocal.hitPlane[1] + rrRayLocal.hitPoint3d[2] * rrRayLocal.hitPlane[2]);
+				}
+				rrRayLocal.hitFrontSide = rrRayLocal.rayDir.dot(rrRayLocal.hitPlane)<0;
+				rrRayLocal.hitObject = rrRayGlobal->hitObject;
+				rrRayLocal.collisionHandler = rrRayGlobal->collisionHandler;
+#endif
+
+				if (!rrRayLocal.collisionHandler->collides(rrRayLocal))
+				{
+					_args->valid[i] = 0;
+				}
+
+				// in callback: "The filter function is further allowed to change the hit and decrease the tfar value of the ray but it should not modify other ray data nor any inactive components of the ray or hit."
+				// so if we ever modify hit or rayLengthMax in our collision handler, we should propagate changese to embree here
+				//RTCRayN_tfar(_args->ray,_args->N,i) = rrRayLocal.rayLengthMax;
+				//...
+			}
 		}
 	}
 
-	static unsigned loadMesh(RTCScene rtcScene, const RRMesh* _mesh)
+	static RTCGeometry loadMesh(const RRMesh* _mesh)
 	{
 		unsigned numTriangles = _mesh->getNumTriangles();
 		unsigned numVertices = _mesh->getNumVertices();
-		unsigned geomId = rtcNewTriangleMesh(rtcScene,RTC_GEOMETRY_STATIC,numTriangles,numVertices);
-
-		// indices
+		RTCGeometry geometry = rtcNewGeometry(s_embreeDevice,RTC_GEOMETRY_TYPE_TRIANGLE);
+		RRMeshArrays* meshArrays = const_cast<RRMeshArrays*>(dynamic_cast<const RRMeshArrays*>(_mesh));
+		if (meshArrays)
 		{
-			//rtcSetBuffer(rtcScene,geomId,RTC_INDEX_BUFFER,rrMesh->triangle,0,sizeof(RRMesh::Triangle));
-			RRMesh::Triangle* buffer = (RRMesh::Triangle*)rtcMapBuffer(rtcScene,geomId,RTC_INDEX_BUFFER);
-			for (unsigned i=0;i<numTriangles;i++)
-				_mesh->getTriangle(i,buffer[i]);
-			rtcUnmapBuffer(rtcScene,geomId,RTC_INDEX_BUFFER);
+			rtcSetSharedGeometryBuffer(geometry,RTC_BUFFER_TYPE_INDEX,0,RTC_FORMAT_UINT3,meshArrays->triangle,0,sizeof(rr::RRMesh::Triangle),numTriangles);
+			rtcSetSharedGeometryBuffer(geometry,RTC_BUFFER_TYPE_VERTEX,0,RTC_FORMAT_FLOAT3,meshArrays->position,0,sizeof(rr::RRVec3),numVertices);
 		}
+		else
+		{
+			// indices
+			rtcSetNewGeometryBuffer(geometry,RTC_BUFFER_TYPE_INDEX,0,RTC_FORMAT_UINT3,sizeof(rr::RRMesh::Triangle),numTriangles);
+			RRMesh::Triangle* buffer1 = (RRMesh::Triangle*)rtcGetGeometryBufferData(geometry,RTC_BUFFER_TYPE_INDEX,0);
+			if (buffer1)
+				for (unsigned i=0;i<numTriangles;i++)
+					_mesh->getTriangle(i,buffer1[i]);
+			rtcUpdateGeometryBuffer(geometry,RTC_BUFFER_TYPE_INDEX,0);
 		
-		// positions
-		{
-			//rtcSetBuffer(rtcScene,geomId,RTC_VERTEX_BUFFER,rrMesh->position,0,sizeof(RRMesh::Vertex));
-			RRVec3p* buffer = (RRVec3p*)rtcMapBuffer(rtcScene,geomId,RTC_VERTEX_BUFFER);
-			for (unsigned i=0;i<numVertices;i++)
-				_mesh->getVertex(i,buffer[i]);
-			rtcUnmapBuffer(rtcScene,geomId,RTC_VERTEX_BUFFER);
+			// positions
+			rtcSetNewGeometryBuffer(geometry,RTC_BUFFER_TYPE_VERTEX,0,RTC_FORMAT_FLOAT3,sizeof(rr::RRVec3p),numVertices);
+			RRVec3p* buffer2 = (RRVec3p*)rtcGetGeometryBufferData(geometry,RTC_BUFFER_TYPE_VERTEX,0);
+			if (buffer2)
+				for (unsigned i=0;i<numVertices;i++)
+					_mesh->getVertex(i,buffer2[i]);
+			rtcUpdateGeometryBuffer(geometry,RTC_BUFFER_TYPE_VERTEX,0);
 		}
+		rtcSetGeometryIntersectFilterFunction(geometry,intersectFilter);
+		rtcCommitGeometry(geometry);
+		return geometry;
+	}
 
-		rtcSetIntersectionFilterFunction(rtcScene,geomId,callback);
-
-		return geomId;
+	static void errorCallback(void* userPtr, RTCError code, const char* str)
+	{
+		RRReporter::report(ERRO,"Embree: error %d: %hs\n",code,str);
 	}
 
 public:
@@ -157,55 +236,40 @@ public:
 	EmbreeCollider(RRCollider::IntersectTechnique _technique, const RRMesh* _mesh)
 	{
 		if (!s_numEmbreeColliders++)
-			rtcInit(nullptr);
+		{
+			s_embreeDevice = rtcNewDevice(nullptr);
+			rtcSetDeviceErrorFunction(s_embreeDevice,errorCallback,nullptr);
+		}
 
 		rrMesh = _mesh;
-		rtcScene = rtcNewScene( RTC_SCENE_DYNAMIC | ((_technique==IT_BVH_COMPACT) ? (RTC_SCENE_INCOHERENT|RTC_SCENE_COMPACT) : RTC_SCENE_INCOHERENT), RTC_INTERSECT1);
+		rtcScene = rtcNewScene(s_embreeDevice);
 		technique = _technique;
-		loadMesh(rtcScene,_mesh);
-		rtcCommit(rtcScene);
-		// 1) rtcCommit used to be very slow for tiny meshes
-		// it's fast now. making all 23k objects dynamic in diacor takes 8.8s
-		// we can avoid threading overhead completely with rtcCommitThread(rtcScene,0,1), diacor would improve to 7.7s
-		// but building single 8M mesh would be probably much slower, so not worth it
-		// 2) rtcCommit can take 20s and VS2015 debugger can't break during that 20s, no explanation yet, but reboot fixes it
+		RTCGeometry rtcGeometry = loadMesh(_mesh);
+		rtcAttachGeometry(rtcScene,rtcGeometry);
+		rtcCommitScene(rtcScene);
 	}
 
 	virtual bool intersect(RRRay& rrRay) const
 	{
 		if (rrRay.collisionHandler)
 			rrRay.collisionHandler->init(rrRay);
-		Ray rtcRay(rrRay);
-		copyRrRayToRtc(rrRay,rtcRay);
-		
-		// backup rrRay.hitXxx (callback overwrites it, but all intersections can be rejected)
-		RRReal          hitDistance  = rrRay.hitDistance;
-		unsigned        hitTriangle  = rrRay.hitTriangle;
-		RRVec2          hitPoint2d   = rrRay.hitPoint2d;
-		RRVec4          hitPlane     = rrRay.hitPlane;
-		RRVec3          hitPoint3d   = rrRay.hitPoint3d;
-		bool            hitFrontSide = rrRay.hitFrontSide;
+		Ray rtcRayHit(rrRay);
+		copyRrRayToRtc(rrRay,rtcRayHit.ray);
+		rtcRayHit.hit.primID = RTC_INVALID_GEOMETRY_ID; // setting is not mentioned in rtcIntersect1 doc, maybe not necessary
+		rtcRayHit.hit.geomID = RTC_INVALID_GEOMETRY_ID;
+		rtcRayHit.hit.instID[0] = RTC_INVALID_GEOMETRY_ID;
+		RR_ASSERT(RTC_MAX_INSTANCE_LEVEL_COUNT==1); // number of elements in instId array should be 1
 
-		rtcIntersect(rtcScene,rtcRay);
-		bool result = rtcRay.primID!=RTC_INVALID_GEOMETRY_ID;
+		RTCIntersectContextEx rtcIntersectContextEx;
+		rtcInitIntersectContext(&rtcIntersectContextEx);
+		rtcIntersectContextEx.rrRay = &rrRay;
+		rtcIntersect1(rtcScene,&rtcIntersectContextEx,&rtcRayHit);
+		bool result = rtcRayHit.hit.primID!=RTC_INVALID_GEOMETRY_ID;
+		// ? order of next two ifs ?
 		if (rrRay.collisionHandler)
-		{
-			// rrRay.hitXxx is overwritten, here we fix it
-			// (when custom handler rejects collisions, wrong data are left in rrRay.hitXxx)
-			if (result)
-				copyRtcHitToRr(rtcRay,rtcRay.rrRay);
-			else
-			{
-				// restore rrRay.hitXxx (callback overwrites it, but all intersections can be rejected)
-				rrRay.hitDistance = hitDistance;
-				rrRay.hitTriangle = hitTriangle;
-				rrRay.hitPoint2d = hitPoint2d;
-				rrRay.hitPlane = hitPlane;
-				rrRay.hitPoint3d = hitPoint3d;
-				rrRay.hitFrontSide = hitFrontSide;
-			}
 			result = rrRay.collisionHandler->done();
-		}
+		if (result)
+			copyRtcHitToRr(rtcRayHit,rtcRayHit.rrRay);
 		return result;
 	};
 
@@ -227,7 +291,10 @@ public:
 	virtual ~EmbreeCollider()
 	{
 		if (!--s_numEmbreeColliders)
-			rtcExit();
+		{
+			rtcReleaseDevice(s_embreeDevice);
+			s_embreeDevice = nullptr;
+		}
 	};
 };
 
