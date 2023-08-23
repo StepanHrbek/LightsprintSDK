925a5e387931911e9caf8374bc065b39557ca26b
Stepan Hrbek <stepan.git@lightsprint.com>
2023-08-23 21:00:51 +0200

    Improve batch file that generates documentation. (Locate doxygen or help install it, show warnings).

--- doc/doxygen/build.bat
+++ doc/doxygen/build.bat
@@ -1,17 +1,39 @@
-@rem Generate html
-!doxygen\doxygen >log
+@echo off
+rem Attempt to generate html documentation.
 
-@rem Show only namespaces on page namespaces.
+rem Locate doxygen. Give instructions and end if not found.
+set DOXYGEN="C:\Program Files\doxygen\bin\doxygen.exe"
+if not exist %DOXYGEN% (
+    echo Doxygen not found, install it to default location
+    echo {e.g. with commandline: winget install doxygen}
+    echo or tweak location in this .bat if you already have it.
+    exit
+)
+
+rem Delete old documentation.
+rem This is necessary to detect situation, when some file (image)
+rem is no longer copied there, but it still sits there
+rem from previous builds and looks super good.
+if exist ..\html (
+    del /q ..\html\*.*
+)
+
+rem Generate html.
+%DOXYGEN% >nul 2>doxy_warnings.txt
+
+rem If there are warnings/errors, show them and end.
+for /f %%i in ("doxy_warnings.txt") do set SIZE=%%~zi
+if %SIZE% gtr 0 (
+    start doxy_warnings.txt
+    exit
+)
+del doxy_warnings.txt
+
+rem Show only namespaces on page namespaces.
 powershell -Command "(gc ../html/namespaces.html) -replace 'initResizable\(\)', 'initResizable(); toggleLevel(1)' | Out-File -encoding ASCII ../html/namespaces.html"
 
-@rem Make sections font smaller.
+rem Make sections font smaller.
 powershell -Command "(Add-Content ../html/doxygen-awesome.css 'h1 { font-size:130%%; }' )
 
+rem Open generated html.
 start ..\html\index.html
-exit
-
-rem Asi kdyz neni nainstalovane MSDN, hhc.exe nemuze najit hha.dll. Mozna by stacilo misto MSDN nainstalovat jen http://go.microsoft.com/fwlink/?LinkId=14188
-!doxygen\hhc html\index.hhp
-move /y html\index.chm ..\Lightsprint.chm
-start ..\Lightsprint.chm
-rd /s /q html
