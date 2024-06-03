27b23f714e0dcd37de895518b7b91637e210e0e0
Stepan Hrbek <stepan.git@lightsprint.com>
2024-06-03 22:18:05 +0200

    Add temporary workaround for static initialization order fiasco.
    The workaround is still technically undefined behavior.
    I quickly commit this workaround before working on actual API breaking solution.
    
    We have at least two static objects that interact at destruction time,
    s_imageCache dtor may warn about memory leak using g_reporters (which might have been
    already destructed, order is not guaranteed).
    It took two decades before I finally hit this error. One sample crashed on exit(),
    because exit() calls destructors of static objects and with unusual combination
    of linker and build system, static objects destructed in unexpected order.
    This is also a reminder that we should avoid static objects.

--- src/LightsprintCore/RRReporter/RRReporter.cpp
+++ src/LightsprintCore/RRReporter/RRReporter.cpp
@@ -10,16 +10,51 @@
 #include "Lightsprint/RRDebug.h"
 #include <cstdio>
 #include <set>
+#include <typeinfo>
+
 
 namespace rr
 {
 
 /////////////////////////////////////////////////////////////////////////////
 //
+// Reporters
+//
+// This is temporary workaround for our static_initialization_order_fiasco.
+// The workaround is still technically undefined behavior, but it works.
+// I quickly commit this workaround before working on actual API breaking solution.
+// 
+// We have at least two static objects that interact at destruction time,
+// s_imageCache dtor may warn about memory leak using g_reporters (which might have been
+// already destructed, order is not guaranteed).
+// It took two decades before I finally hit this error. One sample crashed on exit(),
+// because exit() calls destructors of static objects and with unusual combination
+// of linker and build system, static objects destructed in unexpected order.
+// This is also a reminder that we should avoid static objects.
+
+class Reporters : public std::set<RRReporter*>
+{
+public:
+	bool valid = false;
+	Reporters()
+	{
+		valid = true;
+	}
+	~Reporters()
+	{
+		valid = false;
+		// when someone accesses destructed g_reporters, it sees valid == false
+		// (as long as no one overwrites the memory)
+	}
+};
+
+/////////////////////////////////////////////////////////////////////////////
+//
 // RRReporter
 
 static int                   g_indentation = 0;
-static std::set<RRReporter*> g_reporters;
+//static std::set<RRReporter*> g_reporters;
+static Reporters             g_reporters;
 bool                         g_typeEnabled[TIMI+1] = {1,1,1,1,1,0,0,1};
 
 RRReporter::RRReporter()
@@ -57,7 +92,7 @@ void RRReporter::indent(int delta)
 
 void RRReporter::reportV(RRReportType type, const char* format, va_list& vars)
 {
-	if (g_reporters.size() && type>=ERRO && type<=TIMI && g_typeEnabled[type])
+	if (g_reporters.size() && g_reporters.valid && type>=ERRO && type<=TIMI && g_typeEnabled[type])
 	{
 		enum {MAX_REPORT_SIZE=1000};
 		char msg[MAX_REPORT_SIZE+1];
