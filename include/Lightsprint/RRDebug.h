e20a008a627dcd5e973a1deef850fa5e9db3ef34
Stepan Hrbek <stepan.git@lightsprint.com>
2024-06-05 10:28:36 +0200

    Add RR_DEBUG(a) as a shortcut for #ifdef _DEBUG a #endif.

--- include/Lightsprint/RRDebug.h
+++ include/Lightsprint/RRDebug.h
@@ -16,8 +16,10 @@
 
 #define RR_LIMITED_TIMES(times_max,action) {static unsigned times_done=0; if (times_done<times_max) {times_done++;action;}}
 #ifdef _DEBUG
+	#define RR_DEBUG(a) a
 	#define RR_ASSERT(a) {if (!(a)) RR_LIMITED_TIMES(10,rr::RRReporter::assertionFailed(#a,__FUNCTION__,__FILE__,__LINE__));}
 #else
+	#define RR_DEBUG(a)
 	#define RR_ASSERT(a) 0
 #endif
 #ifdef _MSC_VER
